d09c14cf7938ac787a18cee486d5ef9ce23853b3
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-09-22 20:14:25 +0300

    Refactor Header_description component to use font-extrabold for the title

--- components/Header_description.tsx
+++ components/Header_description.tsx
@@ -4,7 +4,7 @@ import { Button } from './ui/button'
 const Header_description = () => {
   return (
     <div className='flex flex-col space-y-4'>
-      <h1 className='text-6xl text-[#5AC35A] font-bold'>Unlock your dream career.</h1>
+      <h1 className='text-6xl text-[#5AC35A] font-extrabold'>Unlock Your Dream Career.</h1>
       <p>Discover Your Strengths, Interest, Perfect Career Match And The Business Venture For You</p>
       <Button className='bg-gradient-to-tr from-[#5AC35A] to-[#00AE76] w-fit'>Start Your Journey</Button>
     </div>
