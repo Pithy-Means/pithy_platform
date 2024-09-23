b238fdbd20fb6579596ca975988af4401890af1c
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-09-23 06:24:43 +0300

    Add Whoweare component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
+import Whoweare from '@/components/Whoweare'
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Whoweare />
     </div>
   );
 }
