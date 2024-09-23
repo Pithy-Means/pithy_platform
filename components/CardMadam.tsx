4e1f9c474ee96e7b990a251ab7e24e7ece12d8eb
Doreen Ikilai <dornkaizen@gmail.com>
2024-09-23 09:44:25 +0000

    Refactor CardMadam component to adjust image size and position

--- components/CardMadam.tsx
+++ components/CardMadam.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 const CardMadam = () => {
   return (
     <div className='h-[400px] w-[400px] bg-white'>
-      <div className='bg-[#5AC35A] shadow-inner flex items-center justify-center h-[250px] w-[250px] relative rounded'>
+      <div className='bg-[#5AC35A] shadow-inner flex items-center justify-center h-[300px] w-[300px] relative rounded'>
         <Image
           src='/assets/whoweare.png'
-          height={300}
-          width={200}
+          height={320}
+          width={220}
           alt='Header'
-          className='object-cover absolute top-[-90px] drop-shadow-2xl'
+          className='object-cover absolute top-[-76px] drop-shadow-2xl'
         />
       </div>
     </div>
