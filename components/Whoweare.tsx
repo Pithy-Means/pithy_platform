dd7e54e705321369d1efb8b5ccff012586d65ef1
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-09-23 06:25:04 +0300

    Add Whoweare component to Home page

---
+++ components/Whoweare.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Image from 'next/image'
+import { Button } from './ui/button';
+
+
+const Whoweare = () => {
+  return (
+    <div className='py-20 px-10 bg-white'>
+      <div className='flex justify-between items-center'>
+        <div className='flex flex-col w-2/3 space-y-4'>
+          <div className='flex items-center space-x-2 text-black'>
+            <h1 className='text-4xl text-black font-extrabold'>Who We Are</h1>
+            <span className='bg-gradient-to-t from-[#5AC35A] to-[#00AE76] h-2 w-2 rounded-full'></span>
+          </div>
+          <p className='capitalize text-black'>Empowering informed decisions for a fulfing career</p>
+          <p className='text-black'>At pithy means, we believe that everyone deserves to find their perfect fit. our mission is to guide students, professionals, and business managers towards their most suitable study, professional, or business areas.</p>
+          <div className='flex justify-between'>
+            <div className='flex flex-col space-y-4'>
+              <div className='bg-[#5AC35A] w-fit text-black font-extrabold px-2 rounded-md'>Our Story</div>
+              <p className='capitalize text-black'>Founded in the US and Uganda, our team saw the need to prevent:</p>
+              <div className='flex flex-col ml-4'>
+                <div className='flex space-x-2'>
+                  <div className='border-2 border-[#5AC35A] rotate-45 p-1'></div>
+                  <p className='capitalize text-black'>identify natural strengths and interests</p>
+                </div>
+                <div className='flex space-x-2'>
+                  <div className='border-2 border-[#5AC35A] rotate-45 p-1'></div>
+                  <p className='capitalize text-black'>Explore suitable career and business paths</p>
+                </div>
+                <div className='flex space-x-2'>
+                  <div className='border-2 border-[#5AC35A] rotate-45 p-1'></div>
+                  <p className='capitalize text-black'>Make informed decisions</p>
+                </div>
+              </div>
+            </div>
+            <div className='flex flex-col space-y-4'>
+              <div className='bg-[#5AC35A] w-fit text-black font-extrabold px-2 rounded-md'>Our vision</div>
+              <p className='capitalize text-black'>A world where everyone thrives in their chosen path.</p>
+              <Button className='text-white bg-gradient-to-r from-[#5AC35A] to-[#00AE76] w-fit'>Learn More</Button>
+            </div>
+          </div>
+        </div>
+        <div className='h-[400px] w-[400px] bg-white'>
+        <div className='bg-[#5AC35A] shadow-inner flex items-center justify-center h-[250px] w-[250px] relative rounded'>
+          <Image 
+            src='/assets/whoweare.png' 
+            height={300} 
+            width={200} 
+            alt='Header'
+            className='object-cover absolute top-[-90px] drop-shadow-2xl'
+          />
+        </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Whoweare
