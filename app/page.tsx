dad102be3bb99e96b269ff08529161fa8fec9ec8
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-09-23 09:05:22 +0300

    Add Whatwegive component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Navbar from "@/components/Navbar";
 import Whoweare from '@/components/Whoweare'
+import Whatwegive from '@/components/Whatwegive'
+
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Whoweare />
+      <Whatwegive />
     </div>
   );
 }
