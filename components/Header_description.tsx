78b446a620045fc647cf2cd6580ee8b7b07c59c1
Doreen Ikilai <dornkaizen@gmail.com>
2024-09-30 21:32:44 +0000

    Refactor Header_description component styles

--- components/Header_description.tsx
+++ components/Header_description.tsx
@@ -3,8 +3,8 @@ import { Button } from './ui/button'
 
 const Header_description = () => {
   return (
-    <div className='flex flex-col space-y-4'>
-      <h1 className='text-6xl text-[#5AC35A] font-extrabold'>Unlock Your Dream Career.</h1>
+    <div className='flex flex-col space-y-4 w-1/2'>
+      <h1 className='text-4xl text-[#5AC35A] font-extrabold'>Unlock Your Dream Career.</h1>
       <p>Discover Your Strengths, Interest, Perfect Career Match And The Business Venture For You</p>
       <Button className='bg-gradient-to-tr from-[#5AC35A] to-[#00AE76] w-fit'>Start Your Journey</Button>
     </div>
