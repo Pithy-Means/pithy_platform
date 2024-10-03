05895d7fc7ac297fd1ad8d23b405725eb5026dc4
Doreen Ikilai <dornkaizen@gmail.com>
2024-10-03 19:06:30 +0000

    Refactor ContactInfo component to improve layout and handle multiple phone numbers

--- components/ContactInfo.tsx
+++ components/ContactInfo.tsx
@@ -13,7 +13,7 @@ const ContactInfo: React.FC<ContactInfoProps> = ({ title, location, email, phone
   const phoneNumbers = Array.isArray(phone) ? phone : [phone];
   return (
     <div className={`flex flex-col space-y-4 text-center ${className}`}>
-      <h1 className={`text-lg md:text-xl lg:text-2xl font-bold uppercase  md:text-left ${className}`}>{title}</h1>
+      <h1 className={`text-lg md:text-xl lg:text-2xl font-bold uppercase  md:text-center ${className}`}>{title}</h1>
       <div className='flex flex-col items-center md:flex-row md:items-start space-x-2 md:space-x-4'>
         <CiLocationOn className={`text-lg md:text-xl lg:text-2xl ${className}`} />
         <p className={`text-[12px] md:text-sm lg:text-base text-center md:text-start w-full md:w-auto${className}`}>{location}</p>
@@ -24,26 +24,19 @@ const ContactInfo: React.FC<ContactInfoProps> = ({ title, location, email, phone
           {email}
         </a>
       </div>
-      {/* <div className='flex flex-col items-center md:flex-row md:items-start  md:space-x-4 space-x-2'>
+      <div className={`flex flex-col  md:flex-row md:space-x-4 md:items-start ${className}`}>
         <CiPhone className={`text-lg md:text-xl lg:text-2xl ${className}`} />
-        <a href={`tel:${phone.replace(/\s/g, '')}`} className={`text-[12px] hover:underline md:text-sm lg:text-base text-start ${className}`}>
-          {phone}
-        </a>
-      </div> */}
-      <div className={`flex flex-col  md:flex-row md:items-start ${className}`}>
+        <div className={`flex flex-col items-center justify-center space-y-2 md:w-auto`}>
         {phoneNumbers.map((phone: string, index: number) => (
-          <div key={index} className="flex flex-col items-center space-x-2 md:w-auto">
-            <CiPhone className={`text-lg md:text-xl lg:text-2xl ${className}`} />
             <a
+              key={index}
               href={`tel:${phone.replace(/\s/g, '')}`}
-              className={`text-[12px] hover:underline md:text-sm lg:text-base text-start ${className}`}
+              className={`text-[12px] hover:underline md:text-sm break-normal lg:text-base text-start ${className}`}
             >
               {phone}
             </a>
-            {index < phoneNumbers.length - 1 && (
-              <span className='hidden md:inline'>|</span>)}
-          </div>
         ))}
+        </div>
       </div>
     </div>
   );
