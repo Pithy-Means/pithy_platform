1300f1561b6f8174c3d595a3aa9e2b42c3543780
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-03 12:43:59 +0000

    Refactor CardMadam component layout and styles

--- components/CardMadam.tsx
+++ components/CardMadam.tsx
@@ -2,18 +2,18 @@ import Image from 'next/image';
 
 const CardMadam = () => {
   return (
-    <div className='h-[400px] w-[400px] bg-white'>
+    <div className='bg-white'>
       <div className='bg-[#5AC35A] shadow-inner flex items-center justify-center h-[300px] w-[300px] relative rounded'>
         <Image
           src='/assets/whoweare.png'
           height={320}
           width={220}
           alt='Header'
-          className='object-cover absolute top-[-76px] drop-shadow-2xl'
+          className='object-cover absolute top-[-75px] drop-shadow-2xl'
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CardMadam;
