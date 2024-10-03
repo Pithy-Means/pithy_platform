a5026c0543de5c4609af34eea748dc77bf55d955
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-03 12:44:53 +0000

    Refactor Howdoesitwork component layout and styles for responsiveness

--- components/Howdoesitwork.tsx
+++ components/Howdoesitwork.tsx
@@ -4,9 +4,9 @@ import AssCards from './AssCards';
 const Howdoesitwork = () => {
   return (
     <div className='px-10 py-4 bg-white'>
-      <div className='flex flex-col space-y-2'>
+      <div className='flex flex-col lg:space-y-2 space-y-8 lg:items-start items-center'>
         <TitleDot title={'how does it work?'} />
-        <p className='capitalize text-black'>Discover your ideal career path in 3 easy Steps</p>
+        <p className='capitalize text-black lg:text-start text-center'>Discover your ideal career path in 3 easy Steps</p>
         <AssCards />
       </div>
     </div>
