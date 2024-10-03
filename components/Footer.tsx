677a68bcd4d61a15a63e83c603f2b9af9662c49c
Doreen Ikilai <dornkaizen@gmail.com>
2024-10-03 12:55:03 +0000

    Refactor Footer component to improve layout and responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import QuickLinks from './QuickLinks';
 const Footer = () => {
   return (
     <div className='bg-black'>
-      <div className="flex h-96">
+      <div className="flex flex-col md:flex-row h-auto md:h-96 w-full lg:flex-row">
         <div
           className="flex-1 bg-contain bg-no-repeat bg-left-bottom flex items-center justify-center text-white"
           style={{
@@ -13,15 +13,15 @@ const Footer = () => {
           }}
         />
 
-        <div className='flex py-10 w-4/5 space-x-4'>
-          <div className='flex flex-col w-1/5'>
+        <div className='flex flex-col md:flex-row py-10 w-full md:w-4/5 space-x-4 md:space-x-4'>
+          <div className='flex flex-col w-full md:w-1/5 mb-4 md:mb-0'>
             <div className='flex justify-between'>
               <div className='flex flex-col space-y-4'>
                 <div className='flex space-x-4 items-center'>
                   <div className='p-1 bg-[#5AC35A] h-4 w-4 rounded-full'></div>
                   <p className='text-[#5AC35A] text-lg capitailze'>Pithy means</p>
                 </div>
-                <p className='text-white text-base'>Lorem psum aoka psuma lroe taray aoksa mdak djskas aso</p>
+                <p className='text-white text-base '>Lorem psum aoka psuma lroe taray aoksa mdak djskas aso</p>
                 <SocialMediaLinks className='text-white' />
               </div>
             </div>
@@ -31,14 +31,14 @@ const Footer = () => {
             location={'1309 Coffeen Avenue STE 10269, Sheridan, WY 82801, USA'}
             email={'contact@pithymeans.com'}
             phone={'+1 (307) 374-0993 | +1 (307) 205-5983'}
-            className='text-white'
+            className='text-white w-full  mb-4 md:mb-0 '
           />
           <ContactInfo 
             title={'office uganda'} 
-            location={'Plot No 546, ROFRA house, 4th Floor, Room No 2, Ggaba Road, Kansanga, Kampala.'}
+            location={'Plot No 546, ROFRA house, 4th Floor,Room No 2, Ggaba Road, Kansanga, Kampala.'}
             email={'pithymeansafrica@gmail.com'}
             phone={'+256 750 175 892 | +256 760 389 466 | 783184543'}
-            className='text-white'
+            className='text-white w-full  mb-4 md:mb-0 '
           />
           <QuickLinks />
         </div>
