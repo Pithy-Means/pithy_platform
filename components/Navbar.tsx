92ec4c239d938e3523e6d1b210dd46369b73bcfc
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-04 17:50:37 +0000

    Refactor responsive layout and image imports in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,46 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname} from 'next/navigation'
-import Logo from './Logo'
+import { usePathname } from 'next/navigation';
+import Logo from './Logo';
 import { Button } from './ui/button';
-import { useState } from 'react';
-
-
+import { useState, useEffect, useRef } from 'react';
 
 const Navbar = () => {
   const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const handleToggle = () => {
-    setIsOpen(!isOpen);
-  }
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleCloseMenu = () => {
+    setIsOpen(false);
+  };
+
+  // Close the menu when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    const handleEscapeKey = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('keydown', handleEscapeKey);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('keydown', handleEscapeKey);
+    };
+  }, []);
 
   return (
     <div className='px-10 pt-6 bg-black'>
@@ -43,29 +69,44 @@ const Navbar = () => {
             </svg>
           </button>
         </div>
+
         {/* Links - Hidden on Small Screens, shown on larger */}
         <div className={'hidden lg:flex items-center space-x-6'}>
           <div className='flex space-x-4'>
-            <Link href='/' prefetch={true} className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
+            <Link
+              href='/'
+              prefetch={true}
+              className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
               Home
             </Link>
-
-            <Link href='/about' prefetch={true} className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/about' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
+            <Link
+              href='/about'
+              prefetch={true}
+              className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/about' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
               About
             </Link>
-            <Link href='/how-it-works' prefetch={true} className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/how-it-works' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
+            <Link
+              href='/how-it-works'
+              prefetch={true}
+              className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/how-it-works' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
               How It Works
             </Link>
-            <Link href='/contact' prefetch={true} className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/contact' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
-
+            <Link
+              href='/contact'
+              prefetch={true}
+              className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/contact' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
               Contact
             </Link>
           </div>
           <div className='flex space-x-12'>
             <div className='flex items-center gap-x-2'>
-              <Link href='/signIn' prefetch={true} className='text-[#5AC35A] hover:text-white transition duration-300'>Login</Link>
+              <Link href='/signIn' prefetch={true} className='text-[#5AC35A] hover:text-white transition duration-300'>
+                Login
+              </Link>
               <div className='bg-white h-3 w-0.5'></div>
-              <Link href='#'  prefetch={true}className='text-white hover:text-[#5AC35A] transition duration-300'>Sign Up</Link>
+              <Link href='#' prefetch={true} className='text-white hover:text-[#5AC35A] transition duration-300'>
+                Sign Up
+              </Link>
             </div>
             <div>
               <Button className='bg-[#5AC35A] px-8 py-0'>Take Test</Button>
@@ -76,39 +117,51 @@ const Navbar = () => {
 
       {/* Mobile Menu - Visible only when toggled open */}
       {isOpen && (
-        <div className='lg:hidden flex flex-col space-y-4 mt-4 items-center text-center'>
-          <Link href='/' className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
+        <div
+          ref={menuRef}
+          className='lg:hidden flex flex-col space-y-4 mt-4 items-center text-center animate-slide-in'>
+          <Link
+            href='/'
+            className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}
+            onClick={handleCloseMenu}>
             Home
           </Link>
-
-          <Link href='/about' className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/about' ? 'underline decoration-[#5AC35A] underline-offset-4 decoration-2' : ''}`}>
+          <Link
+            href='/about'
+            className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/about' ? 'underline decoration-[#5AC35A] underline-offset-4 decoration-2' : ''}`}
+            onClick={handleCloseMenu}>
             About
           </Link>
-
-          <Link href='/how-it-works' className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/how-it-works' ? 'underline decoration-[#5AC35A] underline-offset-4 decoration-2' : ''}`}>
+          <Link
+            href='/how-it-works'
+            className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/how-it-works' ? 'underline decoration-[#5AC35A] underline-offset-4 decoration-2' : ''}`}
+            onClick={handleCloseMenu}>
             How It Works
           </Link>
-
-          <Link href='/contact' className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/contact' ? 'underline decoration-[#5AC35A] underline-offset-4 decoration-2' : ''}`}>
+          <Link
+            href='/contact'
+            className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/contact' ? 'underline decoration-[#5AC35A] underline-offset-4 decoration-2' : ''}`}
+            onClick={handleCloseMenu}>
             Contact
           </Link>
-
           <div className='flex flex-col space-y-4'>
             <div className='flex items-center gap-x-2'>
-              <Link href='#' className='text-[#5AC35A] hover:text-white transition duration-300'>Login</Link>
+              <Link href='#' className='text-[#5AC35A] hover:text-white transition duration-300'>
+                Login
+              </Link>
               <div className='bg-white h-3 w-0.5'></div>
-              <Link href='#' className='text-white hover:text-[#5AC35A] transition duration-300'>Sign Up</Link>
+              <Link href='#' className='text-white hover:text-[#5AC35A] transition duration-300'>
+                Sign Up
+              </Link>
             </div>
-
             <div>
               <Button className='bg-[#5AC35A] w-full'>Take Test</Button>
             </div>
           </div>
         </div>
       )}
-    
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
