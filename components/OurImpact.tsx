21dc6ee054c6837aac069f5094630e65266b1b7a
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-04 13:48:43 +0000

    Refactor Image import in OurImpact component

--- components/OurImpact.tsx
+++ components/OurImpact.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import TitleDot from './TitleDot';
 import StarRating from './Stars';
 import { PiQuotesFill } from "react-icons/pi";
