2989358d0e4ae9ce0dcce76e25035c3f5f5a530c
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-04 13:48:30 +0000

    Refactor Navbar component to update link styling and add prefetch attribute

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -46,26 +46,26 @@ const Navbar = () => {
         {/* Links - Hidden on Small Screens, shown on larger */}
         <div className={'hidden lg:flex items-center space-x-6'}>
           <div className='flex space-x-4'>
-            <Link href='/' className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
+            <Link href='/' prefetch={true} className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
               Home
             </Link>
 
-            <Link href='/about' className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/about' ? 'underline decoration-[#5AC35A] underline-offset-4 decoration-2' : ''}`}>
+            <Link href='/about' prefetch={true} className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/about' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
               About
             </Link>
-            <Link href='/how-it-works' className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/how-it-works' ? 'underline decoration-[#5AC35A] underline-offset-4 decoration-2' : ''}`}>
+            <Link href='/how-it-works' prefetch={true} className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/how-it-works' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
               How It Works
             </Link>
-            <Link href='/contact' className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/contact' ? 'underline decoration-[#5AC35A] underline-offset-4 decoration-2' : ''}`}>
+            <Link href='/contact' prefetch={true} className={`text-white hover:text-[#5AC35A] transition duration-300 ${pathname === '/contact' ? 'underline decoration-[#5AC35A] underline-offset-8 decoration-2' : ''}`}>
 
               Contact
             </Link>
           </div>
           <div className='flex space-x-12'>
             <div className='flex items-center gap-x-2'>
-              <Link href='/signIn' className='text-[#5AC35A] hover:text-white transition duration-300'>Login</Link>
+              <Link href='/signIn' prefetch={true} className='text-[#5AC35A] hover:text-white transition duration-300'>Login</Link>
               <div className='bg-white h-3 w-0.5'></div>
-              <Link href='#' className='text-white hover:text-[#5AC35A] transition duration-300'>Sign Up</Link>
+              <Link href='#'  prefetch={true}className='text-white hover:text-[#5AC35A] transition duration-300'>Sign Up</Link>
             </div>
             <div>
               <Button className='bg-[#5AC35A] px-8 py-0'>Take Test</Button>
