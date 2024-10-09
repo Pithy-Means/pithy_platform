a9d8ed00dc4cb1d8bb1b613cfb2cd97bf21dfa40
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-09 05:24:08 +0000

    Refactor naming conventions for database and collections

---
+++ models/name.ts
@@ -0,0 +1,20 @@
+// Parts of naming database name and tables in the database
+export const db = 'ptiydb';
+export const userCollection = 'users';
+export const courseCollection = 'courses';
+export const courseCommentCollection = 'courseComments';
+export const paymentCollection = 'payments';
+export const postCollection = 'posts';
+export const commentCollection = 'comments';
+export const likeCommentCollection = 'likeComments';
+export const likePostCollection = 'likePosts';
+export const preCourseQuestionCollection = 'preCourseQuestions';
+export const preCourseAnswerCollection = 'preCourseAnswers';
+export const postCourseQuestionCollection = 'postCourseQuestions';
+export const postCourseAnswerCollection = 'postCourseAnswers';
+export const certificateCollection = 'certificates';
+export const scholarshipCollection = 'scholarships';
+export const jobCollection = 'jobs';
+export const postAttachementBucket = 'post-attachments';
+export const postCommentCollection = 'postComments';
+export const postLikeCollection = 'postLikes';
