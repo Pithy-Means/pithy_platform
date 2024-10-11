e3623f8a752b4e08ec76fd47cae4b062aa421c26
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-11 07:21:24 +0000

    Refactor InputContact component to use UI components for input and label

--- components/InputContact.tsx
+++ components/InputContact.tsx
@@ -1,10 +1,14 @@
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+
 interface InputContactProps {
   label: string;
-  type?: "text" | "email" | "number" | "tel";
+  type?: "text" | "email" | "number" | "tel" | "password";
   className?: string;
   isTextarea?: boolean;
   value: string;
-  onChange: (
+  onChange?: (
     e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
   ) => void;
 }
@@ -19,16 +23,16 @@ const InputContact: React.FC<InputContactProps> = ({
 }) => {
   return (
     <div>
-      <label className="relative flex items-center">
+      <Label className="relative flex items-center">
         {isTextarea ? (
-          <textarea
+          <Textarea
             value={value}
             onChange={onChange}
             required
-            className={`px-6 h-24 ${className} text-lg bg-white border rounded-lg border-black border-opacity-50 outline-none focus:border-blue-500 focus:text-black transition duration-200`}
+            className={`px-6 h-24 py-4 ${className} text-lg bg-white border rounded-lg border-black border-opacity-50 outline-none focus:border-blue-500 focus:text-black transition duration-200`}
           />
         ) : (
-          <input
+          <Input
             type={type}
             value={value}
             onChange={onChange}
@@ -39,7 +43,7 @@ const InputContact: React.FC<InputContactProps> = ({
         <span className="text-lg text-black text-opacity-80 absolute top-0 left-0 mx-4 py-1 px-2 transition duration-200 input-text">
           {label}
         </span>
-      </label>
+      </Label>
     </div>
   );
 };
