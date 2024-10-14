8106d5d0515878acc912536992c92d039eb5f611
Doreen Ikilai <dornkaizen@gmail.com>
2024-10-14 13:32:05 +0000

    Refactor component : imports and add Dashboard page

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,25 @@
+'useclient'
+import OverView from '@/components/OverView';
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <>
+      <div className=''>
+        <div>
+          <OverView />
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+
+      </div>
+
+    </>
+  )
+}
+
+export default Dashboard;
