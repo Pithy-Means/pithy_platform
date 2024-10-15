c6f902c82d18ffc533b79fe9a028efcdaaf0b667
Doreen Ikilai <dornkaizen@gmail.com>
2024-10-15 18:01:10 +0000

    Refactor dashboard page: Add components for community and person sidebar

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,7 @@
-'useclient'
+// import DashboardLayout from '../layout';
+import Communty from '@/components/communty';
 import OverView from '@/components/OverView';
+import PersonSidebar from '@/components/PersonSidebar';
 
 import ShareSomething from '@/components/ShareSomething';
 import React from 'react'
@@ -7,11 +9,21 @@ import React from 'react'
 function Dashboard() {
   return (
     <>
-      <div className=''>
+      <div className='bg-white/90'>
         <div className='flex h-screen'>
           <OverView />
-          <ShareSomething />
+          <div className='w-full'>
+            <ShareSomething />
+
+          </div>
+         <div className='flex flex-col mr-4'>
+            <PersonSidebar />
+            <Communty />
+
+         </div>
+
          
+
         </div>
 
 
