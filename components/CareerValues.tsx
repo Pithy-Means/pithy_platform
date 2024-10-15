7bec336cb293745cff4289751ec011aa47cba267
Doreen Ikilai <dornkaizen@gmail.com>
2024-10-15 18:00:53 +0000

    Add CareerValues component for displaying career values and interests

---
+++ components/CareerValues.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+type ValuesAndInterestsProps = {
+  values: string[];
+  interests: string[];
+};
+
+const ValuesAndInterests: React.FC<ValuesAndInterestsProps> = ({ values, interests }) => {
+  return (
+    <div>
+      <div>
+        <p className='text-base font-bold mb-2 flex justify-center items-center'>Career values & Interests</p>
+        <div className='flex flex-wrap gap-2 mb-4'>
+          {values.map((value: string, index: number) => (
+            <button
+              key={`value-${index}`}
+              className=''
+            >
+              {value}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div>
+    
+        <div className='flex flex-wrap gap-2'>
+          {interests.map((interest: string, index: number) => (
+            <button
+              key={`interest-${index}`}
+              className=''
+            >
+              {interest}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ValuesAndInterests;
