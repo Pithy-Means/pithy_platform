5adea676a048bab22fc88e4345b0598c1d1c846b
Doreen Ikilai <dornkaizen@gmail.com>
2024-10-16 09:55:22 +0000

    Refactor course page: Add initial implementation
    This commit adds the initial implementation of the course page. It includes the import of the `OverView` component and renders it within a container. The page is currently a work in progress and will be further developed in future commits.

---
+++ app/(dashboard)/course/page.tsx
@@ -0,0 +1,18 @@
+import OverView from '@/components/OverView'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div className='bg-white/90'>
+      <div className='flex h-screen'>
+        <OverView />
+
+      </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default page
