04db1c7c5b843d02469aa1382cdf23c13dfaa999
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-17 09:44:42 +0000

    Refactor sign-up page to use SignupForm component

--- app/(auth)/signUp/page.tsx
+++ app/(auth)/signUp/page.tsx
@@ -1,11 +1,9 @@
-import React from "react";
+import SignupForm from "@/components/SignupForm";
 
-function Signup() {
-  return (
-    <div>
-      <h1>Signup</h1>
-    </div>
-  );
+const Signup = () => {
+    return (
+        <SignupForm />
+    )
 }
 
 export default Signup;
