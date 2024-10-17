d226257f4177e8ceb3f8c237530b7d11f77e160f
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-17 09:46:20 +0000

    Refactor appwrite.ts to add createSessionClient and createAdminClient functions

---
+++ utils/appwrite.ts
@@ -0,0 +1,42 @@
+import env from '@/env';
+import { Client, Databases, Account, Users } from 'node-appwrite';
+import { cookies } from "next/headers";
+
+export async function createSessionClient() {
+  const client = new Client()
+    .setEndpoint(env.appwrite.endpoint)
+    .setProject(env.appwrite.projectId);
+
+  const session = cookies().get("my-session");
+  if (!session || !session.value) {
+    throw new Error("No session");
+  }
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
+
+export const createAdminClient = () => {
+  const client = new Client();
+  client
+    .setEndpoint(env.appwrite.endpoint)
+    .setProject(env.appwrite.projectId)
+    .setKey(env.appwrite.apiKey);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get user() {
+      return new Users(client);
+    }
+  };
+};
