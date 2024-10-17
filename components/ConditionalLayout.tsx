e523f76208a852dc98daa5dd9a687fdabc0a098b
Doreen Ikilai <dornkaizen@gmail.com>
2024-10-17 13:00:42 +0000

    Refactor ConditionalLayout component to update navigation bar based on the current path

--- components/ConditionalLayout.tsx
+++ components/ConditionalLayout.tsx
@@ -7,13 +7,13 @@ import DashboardNavBar from "@/components/dashboard_navBar";
 const ConditionalLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
 
+  const isViewCourse = pathname?.startsWith("/viewcourse");
   const isCourse = pathname?.startsWith("/course");
   const isDashboard = pathname?.startsWith("/dashboard");
 
   return (
     <div>
-      {/* If the path starts with "/dashboard/course", use DashboardNavBar */}
-      {isCourse ? <DashboardNavBar /> : isDashboard ? <DashboardNavBar /> : <Navbar />}
+      {isCourse ? <DashboardNavBar /> : isViewCourse ? < DashboardNavBar/> :isDashboard ? <DashboardNavBar /> : <Navbar />}
       {/* {isDashboard ? <DashboardNavBar /> : <Navbar />} */}
      
       
