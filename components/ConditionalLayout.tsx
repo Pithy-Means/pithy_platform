a3e94253e57d1c8dd56fb53fea2103d4c28fc7d1
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-22 12:54:38 +0000

    Refactor ConditionalLayout component to conditionally render Navbar based on the current pathname

--- components/ConditionalLayout.tsx
+++ components/ConditionalLayout.tsx
@@ -2,21 +2,16 @@
 
 import { usePathname } from "next/navigation";
 import Navbar from "@/components/Navbar";
-import DashboardNavBar from "@/components/dashboard_navBar";
 
 const ConditionalLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
-    
+
   // Determine if the current pathname matches the desired routes
   const isPublicRoute = ["/", "/about", "/contact", "/how-it-works"].includes(pathname);
 
-  const isDashboard = pathname?.startsWith("/dashboard");
-
   return (
-    <div>    
-      {isDashboard ? (
-        <DashboardNavBar />
-      ) : isPublicRoute ? (
+    <div>
+      {isPublicRoute ? (
         <Navbar />
       ) : ''} {/* Render nothing if not on the specified routes */}
       {children}
