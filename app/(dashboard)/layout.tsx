78fd483142c4d66761d3b4fd588a9df06ba62b9e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-22 20:31:40 +0000

    Refactor DashboardLayout to fetch and display the logged-in user's first name initial in the navbar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,11 @@
-'use client';
-
 import DashboardNavBar from "@/components/dashboard_navBar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-export default function DashboardLayout ({ children }: {children: React.ReactNode }) {
- 
+export default async function DashboardLayout ({ children }: {children: React.ReactNode }) {
+  const loggedIn = await getLoggedInUser();
   return (
-    <div className=" ">
-      <DashboardNavBar>
+    <div className="">
+      <DashboardNavBar user={loggedIn.firstname.charAt(0).toUpperCase()}>
         {children}
       </DashboardNavBar>
     </div>
