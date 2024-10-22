3fa870dc9cfb0975bf266e0137d2eff820cd211d
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-22 20:32:27 +0000

    Add Payment page with PaymentForm component

---
+++ app/payment/page.tsx
@@ -0,0 +1,12 @@
+import PaymentForm from '@/components/PaymentForm'
+import React from 'react'
+
+const Payment = () => {
+  return (
+    <div>
+      <PaymentForm />
+    </div>
+  )
+}
+
+export default Payment
