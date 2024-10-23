2ce002ef0e9ce0b48038ab957c5a8f717bc5aa4c
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-23 05:28:35 +0000

    Refactor OverView component to update the layout and styling

--- components/OverView.tsx
+++ components/OverView.tsx
@@ -10,25 +10,25 @@ import { IoMdLogOut } from "react-icons/io";
 
 const OverView = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="">
-      <div className="flex flex-col bg-white text-black px-6 justify-center  mt-6">
+    <div className="flex">
+      <div className="flex flex-col bg-white text-black px-6 justify-center rounded mt-6 shadow-lg shadow-black w-1/4 ">
         <div className="">
           <div>
             <p className="text-lg py-4">Overview</p>
             <div className="space-y-4 mb-10">
-              <div className="flex flex-row gap-1">
+              <div className="flex flex-row gap-3">
                 <GoHome size={24} />
                 <p className="text-base">Home</p>
               </div>
-              <div className="flex flex-row gap-1 items-center">
+              <div className="flex flex-row gap-3 items-center">
                 <HiMiniClipboardDocumentList size={24} />
                 <p className="text-base">courses</p>
               </div>
-              <div className="flex flex-row gap-1 items-center">
+              <div className="flex flex-row gap-3 items-center">
                 <IoIosPeople size={24} />
                 <p className="text-base">Community</p>
               </div>
-              <div className="flex flex-row gap-1 items-center">
+              <div className="flex flex-row gap-3 items-center">
                 <MdOutlineAddCircle size={24} />
                 <p className="text-base">Post</p>
               </div>
@@ -37,38 +37,39 @@ const OverView = ({ children }: { children: React.ReactNode }) => {
         </div>
 
         <div>
-          <div>
+          <>
             <div>
               <p className="text-lg py-4">Account</p>
             </div>
             <div className="space-y-4 mb-10">
-              <div className="flex flex-row gap-1 items-center">
+              <div className="flex flex-row gap-3 items-center">
                 <IoPersonOutline size={24} />
                 <p className="text-base">Profile & settings</p>
               </div>
-              <div className="flex flex-row gap-1 items-center">
+              <div className="flex flex-row gap-3 items-center">
                 <IoNotifications size={24} />
                 <p className="text-base">Notifications</p>
               </div>
             </div>
-          </div>
+          </>
         </div>
         <div>
           <div>
             <p className="text-lg py-2">other features</p>
           </div>
           <div className="space-y-4 mb-2">
-            <div className="flex flex-row gap-1 items-center">
+            <div className="flex flex-row gap-3 items-center">
               <IoMdHelpCircleOutline size={24} />
               <p className="text-base">Help & support</p>
             </div>
-            <div className="flex flex-row gap-1 items-center">
+            <div className="flex flex-row gap-3 items-center text-[#F26900]">
               <IoMdLogOut size={24} />
               <p className="text-base">Logout</p>
             </div>
           </div>
         </div>
       </div>
+      {children}
     </div>
   );
 };
