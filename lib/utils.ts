f4cccb88cb96aa2f357e21863dcf1bb7709103e0
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-23 07:06:25 +0000

    Refactor utils.ts to add generateValidPostId function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,14 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { v4 as uuidv4 } from 'uuid';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
+
+export const generateValidPostId = (post_id?: string): string => {
+  const isValidPostId = post_id && /^[a-zA-Z0-9._-]{1,36}$/.test(post_id);
+  return isValidPostId ? post_id : uuidv4();
+};
