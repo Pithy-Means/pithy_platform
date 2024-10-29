5508088b9d62ac24702842cf867a8f7055b743bc
Doreen Ikilai <dornkaizen@gmail.com>
2024-10-29 15:35:10 +0000

    Refactor OverView component to fix nesting issue in courses page

--- app/(dashboard)/courses/page.tsx
+++ app/(dashboard)/courses/page.tsx
@@ -7,13 +7,14 @@ const page = () => {
   return (
     <div>
       <div className='bg-white/90'>
-      <div className='flex h-screen'>
-        <OverView />
-        <div className='w-full'>
-          <CourseView />
-         
+        <div className='flex h-screen'>
+          <OverView >
+
+            <div className='w-full'>
+              <CourseView />
+            </div>
+          </OverView>
         </div>
-      </div>
 
       </div>
     </div>
