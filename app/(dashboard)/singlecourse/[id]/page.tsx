d6f2290504bfa050304531dc2ad361e588335b01
Doreen Ikilai <dornkaizen@gmail.com>
2024-10-29 15:35:23 +0000

    Refactor SingleCourse component to fix nesting issue in courses page and add content property to Module type

--- app/(dashboard)/singlecourse/[id]/page.tsx
+++ app/(dashboard)/singlecourse/[id]/page.tsx
@@ -11,6 +11,7 @@ import OtherCourses from '@/components/OtherCourses';
 type Module = {
   title: string;
   isLocked: boolean;
+  content: string;
 };
 
 const SingleCourse: React.FC = () => {
@@ -153,7 +154,7 @@ const SingleCourse: React.FC = () => {
     setModules((prevModules) =>
       prevModules.map((module) =>
         module.title === moduleTitle
-          ? { ...module, isLocked: false } 
+          ? { ...module, isLocked: false }
           : module
       )
     );
@@ -163,75 +164,77 @@ const SingleCourse: React.FC = () => {
   return (
     <div className='flex flex-row bg-gray-300 pr-6 w-full'>
 
-      <OverView />
+      <OverView>
 
-      <div className='flex flex-col space-y-4 p-4 text-black w-full'>
-        <div className=''>
-          <h1 className='text-2xl font-semibold mb-2'>Introduction</h1>
-          <p className='text-gray-500 mb-2'>{modules[0].title}</p>
-          <div>
-            <Image src={course.image}
-              alt={course.title}
-              width={500}
-              height={300}
-              className='object-cover rounded-none w-full h-80'
-            />
-          </div>
-          <div className='flex flex-col'>
-            <div className='flex items-center space-x-4'>
-              <div>
-                <p
-                  style={{ cursor: 'pointer', fontWeight: showSummary ? 'bold' : 'normal' }}
-                  onClick={handleToggleSummary}
-                >
-                  Summary
-                </p>
-              </div>
-
-              <div className='flex flex-col '>
-                <p className='px-4'
-                  style={{ cursor: 'pointer', fontWeight: showResources ? 'bold' : 'normal' }}
-                  onClick={handleToggleResources}
-                >
-                  Resources
-                </p>
-              </div>
+        <div className='flex flex-col space-y-4 p-4 text-black w-full'>
+          <div className=''>
+            <h1 className='text-2xl font-semibold mb-2'>Introduction</h1>
+            <p className='text-gray-500 mb-2'>{modules[0].title}</p>
+            <div>
+              <Image src={course.image}
+                alt={course.title}
+                width={500}
+                height={300}
+                className='object-cover rounded-none w-full h-80'
+              />
             </div>
-            <div className='flex items-center space-x-4'>
-              {showSummary && (
+            <div className='flex flex-col'>
+              <div className='flex items-center space-x-4'>
                 <div>
-                  <p className='text-base '>{course.description}</p>
+                  <p
+                    style={{ cursor: 'pointer', fontWeight: showSummary ? 'bold' : 'normal' }}
+                    onClick={handleToggleSummary}
+                  >
+                    Summary
+                  </p>
                 </div>
-              )}
-              {showResources && (
-                <div className='text-base xl:text-lg px-4 py-6 space-y-2'>
-                  <p>{course.title}</p>
-                  <p>{course.title}</p>
-                  <p>Resource 3</p>
-                  <p>module title</p>
-                </div>
-              )}
-            </div>
-          </div>
 
+                <div className='flex flex-col '>
+                  <p className='px-4'
+                    style={{ cursor: 'pointer', fontWeight: showResources ? 'bold' : 'normal' }}
+                    onClick={handleToggleResources}
+                  >
+                    Resources
+                  </p>
+                </div>
+              </div>
+              <div className='flex items-center space-x-4'>
+                {showSummary && (
+                  <div>
+                    <p className='text-base '>{course.description}</p>
+                  </div>
+                )}
+                {showResources && (
+                  <div className='text-base xl:text-lg px-4 py-6 space-y-2'>
+                    <p>{course.title}</p>
+                    <p>{course.title}</p>
+                    <p>Resource 3</p>
+                    <p>module title</p>
+                  </div>
+                )}
+              </div>
+            </div>
+
+          </div>
         </div>
-      </div>
-      <div>
-        <CourseSidebar
-          modules={modules}
-          activeModule={activeModule}
-          setActiveModule={(moduleTitle) => {
-            const selectedModule = module.find((m: Module) => m.title === moduleTitle);
-            if (selectedModule && selectedModule.isLocked) {
-              handleUnlockModule(moduleTitle);
-            } else {
-              setActiveModule(moduleTitle);
-            }
-          }}
-          progress={progress()}
-        />
-        <OtherCourses courses={mockCourses} />
-      </div>
+        <div>
+          <CourseSidebar
+            modules={modules}
+            activeModule={activeModule}
+            setActiveModule={(moduleTitle) => {
+              const selectedModule = modules.find((m: Module) => m.title === moduleTitle);
+              if (selectedModule && selectedModule.isLocked) {
+                handleUnlockModule(moduleTitle);
+              } else {
+                setActiveModule(moduleTitle);
+              }
+            }}
+            progress={progress()}
+            unlockModule={handleUnlockModule}
+          />
+          <OtherCourses courses={mockCourses} />
+        </div>
+      </OverView>
     </div>
   );
 
