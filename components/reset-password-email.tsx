42a2170a10c874e96521e6c245201bf7bd115e51
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-11-06 08:20:50 +0000

    Add ResetPasswordEmail component for sending password reset emails

---
+++ components/reset-password-email.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface ResetPasswordEmailProps {
+  email: string;
+  resetPasswordToken: string;
+}
+
+const ResetPasswordEmail: React.FC<ResetPasswordEmailProps> = ({
+  email,
+  resetPasswordToken,
+}) => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-2xl font-bold">
+        Reset Password <b>${email}</b>
+      </h1>
+      <p className="text-sm">Click the link below to reset your password</p>
+      <a
+        href={`http://localhost:3000/reset-password?resetToken=${resetPasswordToken}`}
+        className="text-blue-500 underline"
+      >
+        Reset Password
+      </a>
+    </div>
+  );
+};
+
+export default ResetPasswordEmail;
