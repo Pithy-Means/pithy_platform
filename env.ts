b472178e817c0f7bc633c1e7335a7ada088796ea
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-11-06 07:56:41 +0000

    Add Resend API key configuration to env.ts

--- env.ts
+++ env.ts
@@ -3,6 +3,9 @@ const env = {
     endpoint: String(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT),
     projectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
     apiKey: String(process.env.APPWRITE_API_KEY)
+  },
+  emails: {
+    apikey: String(process.env.RESEND_API_KEY)
   }
 }
 
