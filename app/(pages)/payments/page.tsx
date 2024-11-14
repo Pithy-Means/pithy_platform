ee10b629509d7c69f7f1b3821bd50c2f12cf7d35
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-11-14 10:26:27 +0000

    Add PaymentForm component to handle Flutterwave payments

---
+++ app/(pages)/payments/page.tsx
@@ -0,0 +1,12 @@
+import Flutter from '@/lib/actions/flutterwave/flutter.action'
+import React from 'react'
+
+const PaymentForm = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Flutter />
+    </div>
+  )
+}
+
+export default PaymentForm
