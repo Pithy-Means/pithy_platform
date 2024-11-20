65c60170380bdc96917ce8712b984dedc79e6d1a
Doreen Ikilai <dornkaizen@gmail.com>
2024-11-20 09:19:16 +0000

    Refactor course data handling by removing dummy data and importing from types

--- app/(api)/api/courses/route.ts
+++ app/(api)/api/courses/route.ts
@@ -1,40 +1,6 @@
 import { NextResponse } from "next/server";
-import course from "@/types/Course";
-
-// Dummy data for testing
-const courses: course[] = [
-  {
-    _id: '1',
-    title: 'Entrepreneurship in East Africa - Case study UG',
-    image: '/assets/development1.png',
-    duration: '1hr - 13min',
-    learners: 93,
-    originalPrice: 'UGX80,000',
-    price: 'Free',
-    description: 'This course is designed to help you understand the basics of entrepreneurship in East Africa, with a focus on Uganda. It covers the key concepts, challenges, and opportunities for entrepreneurs in the region.',
-  },
-  {
-    _id: '2',
-    title: 'Introduction to Web Development',
-    image: '/assets/development2.png',
-    duration: '2hr - 30min',
-    learners: 150,
-    originalPrice: 'UGX100,000',
-    price: 'Free',
-    description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consectetur Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus.'
-  },
-  {
-    _id: '3',
-    title: 'Introduction to Web Development',
-    image: '/assets/development2.png',
-    duration: '2hr - 30min',
-    learners: 150,
-    originalPrice: 'UGX100,000',
-    price: '80,000',
-    description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consectetur Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus.'
-  },
-
-];
+// import course from "@/types/Course";
+import {courses} from "@/types/courses";
 
 // Route to fetch courses
 export async function GET() {
@@ -45,18 +11,3 @@ export async function GET() {
     return new NextResponse('Internal Server Error', { status: 500 });
   }
 };
-
-export const getData = async () => {
-  const res = await fetch('/api/courses');
-  if (!res.ok) {
-    throw new Error('Something went wrong while fetching the data');
-  }
-  const data = await res.json();
-  return data;
-};
-
-export const getSingleCourse = async (_id: number | string): Promise<course | undefined> => {
-  const items = await getData();
-  const singleCourse = items.find((course: course) => course._id === _id);
-  return singleCourse;
-}
