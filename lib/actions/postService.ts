92787352a104e1ac280259cdeb2cda376c961b0d
Doreen Ikilai <dornkaizen@gmail.com>
2024-11-20 08:58:15 +0000

    Refactor fetchPosts and subscribeToPostChanges for improved type safety and clarity

--- lib/actions/postService.ts
+++ lib/actions/postService.ts
@@ -1,14 +1,20 @@
 import { client } from "@/models/client/config";
 import { getPosts, getUserInfo } from "./user.actions";
 import { db, postCollection } from "@/models/name";
+import {  Post } from "@/types/schema";
+
+interface SubscriptionEvent<Tpayload = string> {
+  events: string[];
+  payload: Tpayload;
+};
 
 export const fetchPosts = async () => {
   try {
     const response  = await getPosts();
     console.log('Posts:', response);
     const postsWithUserInfo = await Promise.all(response.documents.map(
-      async (post: { user_id: any; }) => {
-        const user = await getUserInfo({ userId: post.user_id });
+      async (post: { user_id: unknown; }) => {
+        const user = await getUserInfo({ userId: post.user_id  as string});
         return {
           ...post,
           user
@@ -24,13 +30,27 @@ export const fetchPosts = async () => {
 };
 
 // Adjusted subscription logic
-export const subscribeToPostChanges = (callback: (posts: any) => void): (() => void) => {
+// export const subscribeToPostChanges = (callback: (posts: PostWithUser) => void): (() => void) => {
+//   const unsubscription = client.subscribe(
+//     `databases.${db}.collections.${postCollection}.documents`,
+//     callback
+//   );
+
+//   return () => {
+//     unsubscription();
+//   }
+// };
+
+
+export const subscribeToPostChanges = (
+  callback: (event: SubscriptionEvent<Post>) => void
+): (() => void) => {
   const unsubscription = client.subscribe(
-    `databases.${db}.collections.${postCollection}.documents`,
+    `databases.${db}.documents.${postCollection}.documents`,
     callback
   );
 
   return () => {
     unsubscription();
-  }
+  };
 };
