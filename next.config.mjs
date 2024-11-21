e193c17c931f233fd6fed429701013278a6d3450
Doreen Ikilai <dornkaizen@gmail.com>
2024-11-21 12:57:23 +0000

    Enhance Next.js configuration with bundle analyzer and filesystem caching

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// import path from 'path';
+import withBundleAnalyzer from '@next/bundle-analyzer';
+const bundleAnalyzer = withBundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
+});
+const nextConfig = {
+    reactStrictMode: true,
+    // webpack5: true,
+    swcMinify: true,
+   
+    webpack: (config) => {
+        config.cache = {
+            type: 'filesystem',
+            buildDependencies: {
+                config: [__filename],
+            },
+            // cacheDirectory: path.resolve(__dirname, '.next/cache/webpack'),
+        };
+        return config;
+    },
+};
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig);
