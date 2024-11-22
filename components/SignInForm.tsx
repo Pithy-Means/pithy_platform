bab31d7d727a54a265885d1f3d1508d479ed4cab
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-11-22 10:27:13 +0000

    Refactor SignInForm component for improved layout and responsiveness

--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -73,18 +73,18 @@ const SignInForm = () => {
 
   return (
     <div className="flex items-center justify-center w-full">
-      <div className="bg-gradient-to-r from-[#ffffff] via-green-300 to-green-100 p-8 rounded-lg shadow-lg w-full max-w-m h-screen">
-        <div className="flex justify-center space-x-4 items-center w-full">
-          <div className="flex flex-col w-2/4 px-10 mx-auto">
-            <h2 className="text-3xl font-bold text-[#111111] mb-6 capitalize">
+      <div className="bg-gradient-to-r from-[#ffffff] via-green-300 to-green-100 p-8 rounded-lg shadow-lg w-full h-screen">
+        <div className="flex justify-center items-center my-auto space-x-4 w-full md:flex-row flex-col">
+          <div className="flex flex-col w-full lg:w-2/4 px-0 mx-0 lg:px-10 lg:mx-auto">
+            <h2 className="text-xl lg:text-3xl 2xl:text-6xl font-bold text-[#111111] mb-6 capitalize">
               Welcome back
             </h2>
-            <form className="space-y-6 flex flex-col " onSubmit={handleSignIn}>
+            <form className="space-y-6 flex flex-col" onSubmit={handleSignIn}>
               <InputContact
                 label="Email"
                 type="email"
                 name="email"
-                className="w-3/4"
+                className="w-full lg:w-3/4 2xl:py-6 2xl:text-3xl"
                 value={formdata.email as string}
                 onChange={handleChange}
               />
@@ -92,7 +92,7 @@ const SignInForm = () => {
                 <InputContact
                   label="Password"
                   type={showPassword ? "text" : "password"}
-                  className="w-3/4"
+                  className="w-full lg:w-3/4 2xl:py-6 2xl:text-3xl"
                   name="password"
                   value={formdata.password as string}
                   onChange={handleChange}
@@ -100,7 +100,7 @@ const SignInForm = () => {
                 <button
                   type="button"
                   onClick={togglePasswordVisibility}
-                  className="absolute inset-y-0 right-[150px] flex items-center px-2 text-gray-500 hover:text-gray-700"
+                  className="absolute inset-y-0 right-0 lg:right-[150px] 2xl:right-[300px] flex items-center px-2 text-gray-500 hover:text-gray-700"
                 >
                   {showPassword ? "Hide" : "Show"}
                 </button>
@@ -120,7 +120,7 @@ const SignInForm = () => {
               <button
                 type="submit"
                 disabled={loading}
-                className="w-3/4 py-2 px-4 bg-[#3b82f6] text-white font-semibold rounded-md hover:bg-[#2563eb] transition duration-200"
+                className="w-full lg:w-3/4 2xl:py-4 2xl:text-3xl py-2 px-4 bg-[#3b82f6] text-white font-semibold rounded-md hover:bg-[#2563eb] transition duration-200"
               >
                 {loading ? "Signing In ...." : "Sign In"}
               </button>
@@ -129,20 +129,22 @@ const SignInForm = () => {
             <div className="mt-2">
               <Link
                 href="/forgot-password"
-                className="text-sm text-gray-800 hover:text-green-600 transition duration-200"
+                className="text-sm 2xl:text-3xl text-gray-800 hover:text-green-600 transition duration-200"
               >
                 Forgot your password?
               </Link>
             </div>
 
             <div className="">
-              <Link
-                href="/signUp"
-                className="text-sm text-gray-800 hover:text-black hover:font-semibold transition duration-400"
-              >
-                Don&apos;t have an account?{" "}
-                <span className="text-blue-800">Sign up</span>
-              </Link>
+              <li className="text-sm 2xl:text-3xl text-gray-800 list-none">
+                Don&apos;t have an account?
+                <Link
+                  href="/signUp"
+                  className="mx-1 hover:font-semibold transition duration-400"
+                >
+                  <span className="text-blue-800">Sign up</span>
+                </Link>
+              </li>
             </div>
 
             {attempts >= 3 && attempts < MAX_ATTEMPTS && (
@@ -157,14 +159,16 @@ const SignInForm = () => {
             )}
           </div>
 
-          <div className="w-2/4 py-10 px-10 mt-6 md:mt-0 flex justify-center glass-effect">
-            <Image
-              src="/assets/sign.png"
-              alt="Sign In"
-              width={400}
-              height={500}
-              className="max-w-full h-auto object-fill md:max-h-[500px]"
-            />
+          <div className="w-full lg:w-2/4 p-10 md:mt-0 hidden md:flex justify-center glass-effect h-[calc(100vh-64px)] 2xl:h-[calc(100vh-64px)]">
+            <div className="relative w-full h-full">
+              <Image
+                src="/assets/sign.png"
+                alt="Sign In"
+                layout="fill"
+                objectFit="contain"
+                className="object-contain"
+              />
+            </div>
           </div>
         </div>
       </div>
