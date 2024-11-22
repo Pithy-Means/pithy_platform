6c7c09457fd2e7a46d2eaeebf41c952c3b5d3d8e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-11-22 06:53:53 +0000

    Refactor course data structure for improved readability and consistency

--- app/(api)/api/courses/route.ts
+++ app/(api)/api/courses/route.ts
@@ -4,36 +4,38 @@ import course from "@/types/Course";
 // Dummy data for testing
 const courses: course[] = [
   {
-    _id: '1',
-    title: 'Entrepreneurship in East Africa - Case study UG',
-    image: '/assets/development1.png',
-    duration: '1hr - 13min',
+    _id: "1",
+    title: "Entrepreneurship in East Africa - Case study UG",
+    image: "/assets/development1.png",
+    duration: "1hr - 13min",
     learners: 93,
-    originalPrice: 'UGX80,000',
-    price: 'Free',
-    description: 'This course is designed to help you understand the basics of entrepreneurship in East Africa, with a focus on Uganda. It covers the key concepts, challenges, and opportunities for entrepreneurs in the region.',
+    originalPrice: "UGX80,000",
+    price: "Free",
+    description:
+      "This course is designed to help you understand the basics of entrepreneurship in East Africa, with a focus on Uganda. It covers the key concepts, challenges, and opportunities for entrepreneurs in the region.",
   },
   {
-    _id: '2',
-    title: 'Introduction to Web Development',
-    image: '/assets/development2.png',
-    duration: '2hr - 30min',
+    _id: "2",
+    title: "Introduction to Web Development",
+    image: "/assets/development2.png",
+    duration: "2hr - 30min",
     learners: 150,
-    originalPrice: 'UGX100,000',
-    price: 'Free',
-    description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consectetur Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus.'
+    originalPrice: "UGX100,000",
+    price: "Free",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consectetur Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consecteturLorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus.",
   },
   {
-    _id: '3',
-    title: 'Introduction to Web Development',
-    image: '/assets/development2.png',
-    duration: '2hr - 30min',
+    _id: "3",
+    title: "Introduction to Web Development",
+    image: "/assets/development2.png",
+    duration: "2hr - 30min",
     learners: 150,
-    originalPrice: 'UGX100,000',
-    price: '80,000',
-    description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consectetur Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus.'
+    originalPrice: "UGX100,000",
+    price: "80,000",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, doloribus quo nisi quisquam, voluptatem ut suscipit porro ipsam saepe optio fuga nobis consectetur Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus. Voluptatibus, consectetur, adipisicing elit. Labore reiciendis nostrum aut blanditiis possimus.",
   },
-
 ];
 
 // Route to fetch courses
@@ -41,22 +43,24 @@ export async function GET() {
   try {
     return NextResponse.json(courses);
   } catch (error) {
-    console.error('Error fetching courses:', error);
-    return new NextResponse('Internal Server Error', { status: 500 });
+    console.error("Error fetching courses:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
-};
+}
 
 export const getData = async () => {
-  const res = await fetch('/api/courses');
+  const res = await fetch("/api/courses");
   if (!res.ok) {
-    throw new Error('Something went wrong while fetching the data');
+    throw new Error("Something went wrong while fetching the data");
   }
   const data = await res.json();
   return data;
 };
 
-export const getSingleCourse = async (_id: number | string): Promise<course | undefined> => {
+export const getSingleCourse = async (
+  _id: number | string,
+): Promise<course | undefined> => {
   const items = await getData();
   const singleCourse = items.find((course: course) => course._id === _id);
   return singleCourse;
-}
+};
