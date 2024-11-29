7e222996e7641052b3ec5148ea758904a50beb11
Doreen Ikilai <dornkaizen@gmail.com>
2024-11-29 11:22:29 +0000

    Refactor CreatePost component to enhance file handling and validation, integrate image/video compression, and improve error management during file uploads

--- components/createPosts.tsx
+++ components/createPosts.tsx
@@ -1,9 +1,9 @@
+'use client';
 import { createPost } from '@/lib/actions/user.actions';
 import { Post, PostWithUser } from '@/types/schema';
 import React, { useState } from 'react'
-import  setupStorage from '@/models/server/storageSetup';
-import { postAttachementBucket}  from '@/models/server/storageSetup';
-import { Storage} from 'node-appwrite';
+import Image from 'next/image';
+import { isImage, isVideo, compressImage, compressVideo, validateFile} from '../utils/videoImageFormats';
 
 interface CreatePostProps {
   userId: string; // Pass the logged-in user ID as a prop 
@@ -13,30 +13,31 @@ interface CreatePostProps {
 
 const CreatePost: React.FC<CreatePostProps> = ({ userId, onPostCreated }) => {
   // Define initial state for the post
-  const [post, setPost] = useState<Post>({ user_id: userId });
+  const [post, setPost] = useState<Post>(() => ({ user_id: userId }));
   const [loading, setLoading] = useState<boolean>(false);
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [fileType, setFileType] = useState<'image' | 'video' | null>(null);
   const [error, setError] = useState<string | null>(null);
   // const [uploading, setUploading] = useState<boolean>(false);
 
-
   // const storage = setupStorage();
-  const [storage, setStorage] = useState<Storage | null>(null);
-  const [isStorageReady, setIsStorageReady] = useState<boolean>(false);
-  
-  React.useEffect(() => {
-    const initializeStorage = async () => {
-      try{
-        const storageInstance = await setupStorage();
-        setStorage(storageInstance);
-        setIsStorageReady(true);
-      } catch (error) {
-        console.error('Failed to initialize storage:', error);
-      }
-    };
-    initializeStorage();
-  }, []);
+  // const [storage, setStorage] = useState<Storage | null>(null);
+  // const [isStorageReady, setIsStorageReady] = useState<boolean>(false);
+
+  // useEffect(() => {
+  //   const initializeStorage = async () => {
+  //     try{
+  //       const storageInstance = await setupStorage();
+  //       console.log('Storage instance:', storageInstance);
+  //       setStorage(storageInstance);
+  //       setIsStorageReady(true);
+  //     } catch (error) {
+  //       console.error('Failed to initialize storage:', error);
+  //       setError('Failed to initialize storage. Please try again later.');
+  //     }
+  //   };
+  //   initializeStorage();
+  // }, []);
 
   // Handle input change
   const handleInputChange = (
@@ -50,105 +51,80 @@ const CreatePost: React.FC<CreatePostProps> = ({ userId, onPostCreated }) => {
   };
 
   // Handle file change
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
 
-    // Check if the file format is valid
-    const validImageFormats = ['image/jpeg', 'image/png', 'image/gif', 'image/jpg'];
-    const validVideoFormats = ['video/mp4', 'video/webm', 'video/ogg'];
-
-    // Check if the file format is valid
-    if (![...validImageFormats, ...validVideoFormats].includes(file.type)) {
-
-      setError('Invalid file format. please upload JPG, PNG, GIF or MP4 files');
-      setSelectedFile(null);
-      setFileType(null);
+    const validationError = validateFile(file);
+    if (error) {
+      setError(validationError);
       return;
     }
+  
+    try {
+      // let processedFile = file;
+      // if (isImage(file)) {
+      //   processedFile = await compressImage(file);
+      // } else if (isVideo(file)) {
+      //   processedFile = await compressVideo(file);
+      // }
 
-    // Check if the file is an image or video
-    const isImage = validImageFormats.includes(file.type);
-    const isVideo = validVideoFormats.includes(file.type);
-
-    // if (isImage && fileType === 'video') {
-    //   setError('You have already selected a video. please remove it before selecting an image.');
-    //   return;
-    // }
-
-    // if (isVideo && fileType === 'image') {
-    //   setError('You have already selected an image. please remove it before selecting a video.');
-    //   return;
-    // }
-    // Ensure the user is not trying to select both an image and a video at the same time
-    if ((isImage && fileType === 'video') || (isVideo && fileType === 'image')) {
-      setError(`You cannot mix media types. Please remove the current ${fileType} before selecting another.`);
-      return;
-    }
-
-    const url = URL.createObjectURL(file);
-
-    if (isImage) {
-      const image = new Image();
-      image.src = url;
-      image.onload = () => {
-        const { width, height } = image;
-        if (
-          (width === 1080 && height === 1920) || //potrait
-          (width === 1920 && height === 1080) ||//landscape
-          (width === 1080 && height === 1080) //square
-        ) {
-          setSelectedFile(file);
-          setFileType('image');
-          setError(null);
-        } else {
-          setError('Invalid image dimensions.');
-        }
-        URL.revokeObjectURL(url);
-      };
-    } else if (isVideo) {
-      const video = document.createElement('video');
-      video.src = url;
-      video.onloadedmetadata = () => {
-        const { videoWidth, videoHeight } = video;
-        if (
-          (videoWidth === 1080 && videoHeight === 1920) || //potrait
-          (videoWidth === 1920 && videoHeight === 1080) ||//landscape
-          (videoWidth === 1080 && videoHeight === 1080) //square
-        ) {
-          setSelectedFile(file);
-          setFileType('video');
-          setError(null);
-        } else {
-          setError('Invalid video dimensions.');
-        }
-        URL.revokeObjectURL(url);
-      };
+      const processedFile = isImage(file)
+        ? await compressImage(file)
+        : isVideo(file)
+        ? await compressVideo(file)
+        : file;
+  
+      setSelectedFile(processedFile);
+      // setFileType(file.type.includes('image') ? 'image' : 'video');
+      setFileType(isImage(processedFile) ? 'image' : 'video');
+      setError(null);
+    } catch (err) {
+      setError('Error processing file.');
+      throw new Error(`Compression failed: ${err}`);
     }
   };
-
+  
+  // reset 
   const handleRemoveFile = () => {
     setSelectedFile(null);
     setFileType(null);
     setError(null);
   };
 
-  const uploadFileToAppwrite = async (file: File) => {
-    if (!isStorageReady) {
-      setError('Storage is not ready. Please try again later.');
-      return null;
-    };
-    if (!storage) {
-      setError('Failed to upload file. Please try again.');
-      return null;
-    }
-    try {
-      const uploadFile = await storage.createFile(postAttachementBucket, 'unique', file);
-      return uploadFile.$id;
+  const uploadFileToAppwrite = async (file: File): Promise<string | null> => {
+       try {
+      setLoading(true); // Set the loading state
+      const formData = new FormData();
+      formData.append('file', file); // attach the file to the form data
+      // formData.set('file', file); // set the file with the name
+
+      // Send the file to the server
+      const response = await fetch('/api/upload-files', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        const result = await response.json(); // Parse the response body as JSON
+        // const errorData = await response.text(); // Parse the response body as JSON
+        console.error('File upload failed:', result); // Log the error to the console
+        setError(result.error || 'File upload failed. Please try again.'); // Set the error state
+        return null;
+        // handleRemoveFile();
+      }
+
+      const { fileId } = await response.json(); // Extract 'fileId' from the response
+      return fileId; // Return the file 
     } catch (err) {
-      setError('File to upload failed. Please try again.');
+      console.error('File upload failed:', err); // Log the error to the console
+      setError('An unexpected error occurred during file uplaod, please try again.'); // Set the error state
       return null;
-    }
+    } finally { // Reset the loading state
+      setLoading(false);
+    };
+
+
   };
 
   // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -174,34 +150,49 @@ const CreatePost: React.FC<CreatePostProps> = ({ userId, onPostCreated }) => {
     }
 
     try {
-      setLoading(true);
-      const filed = await uploadFileToAppwrite(selectedFile);
-      if (!filed) return;
+      // setLoading(true); //Start loading state
+      // Upload file to Appwrite
+      const fileId = await uploadFileToAppwrite(selectedFile);
+      if (!fileId) {
+        setError('File upload failed. Please check and try again.');
+        return; // If file upload failed, stop further execution
+      }
 
+      // Create the post with the file ID
       const newPost = await createPost({
         ...post, // Include the post content
-        mediaUrl: filed, // Update the post with the media URL
+        mediaUrl: fileId, // Update the post with the media URL
         mediaType: selectedFile.type, // Update the post with the media type
       });
 
-      onPostCreated(newPost); // Update the post list
+      // If post creation failed, stop further execution
+      if (!newPost) {
+        setError('Failed to create post. Please try again.');
+        return;
+      }
+
+      // Post creation successful, update UI
+      onPostCreated(newPost); // Update the post list with new post
+
+      // Reset form state
       setPost({ user_id: userId }); // Reset post
-      setSelectedFile(null); // Reset file
-      setFileType(null); // Reset file type
+      // setSelectedFile(null); // Reset file
+      // setFileType(null); // Reset file type
+      handleRemoveFile(); // optionally handle file removal/reset
     } catch (err) {
-      console.error(err);
-      setError('Failed to create post. Please try again.');
+      console.error('Post creation failed:', err);
+      setError('An unexpected error occure while creating the post.');
     } finally {
-      setLoading(false);
+      setLoading(false); //Reset loading state
     }
   };
 
   return (
     <>
       {loading ? (
-        <p>Creating post...</p>
+        <p>Creating post, please wait...</p>
       ) : (
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-4 text-black">
           <h2 className="text-xl font-semibold mb-4">Create a New Post</h2>
 
           <div className="flex flex-col">
@@ -225,14 +216,31 @@ const CreatePost: React.FC<CreatePostProps> = ({ userId, onPostCreated }) => {
             <input
               type='file'
               id='media'
-              accept='image/jpeg, image/png, image/gif, image/jpg, video/mp4, video/ogg, video/webm'
+              accept='image/*, video/*'
               onChange={handleFileChange}
               className='border border-gray-300 rounded-md p-2'
             />
             {selectedFile && (
               <div className='mt-2'>
-                <p className='text-sm'>
-                  {fileType === 'image' ? 'Image selected.' : 'Video selected.'}
+                <p className='text-sm border dotted rounded-md'>
+                  {fileType === 'image' ? (
+                    <Image src={URL.createObjectURL(selectedFile)}
+                      alt='Selected Image'
+                      width={200}
+                      height={200}
+                      className='max-w-full max-h-64 rounded-md'
+                    />
+                  ) : (
+                    <video
+                      controls
+                      src={URL.createObjectURL(selectedFile)}
+                      // alt='Selected Video'
+                      width={200}
+                      height={200}
+                      className='max-w-full max-h-64 rounded-md'
+                    // controls
+                    />
+                  )}
                 </p>
                 <button
                   type='button'
@@ -250,8 +258,8 @@ const CreatePost: React.FC<CreatePostProps> = ({ userId, onPostCreated }) => {
           <button
             type="submit"
             className={`bg-blue-500 text-white rounded-md py-2 hover:bg-blue-600
-              ${!isStorageReady ? 'cursor-not-allowed opacity-50' : ''}`}
-            disabled={!isStorageReady || loading}
+              ${loading ? 'cursor-not-allowed opacity-50' : ''}`}
+            disabled={loading}
           >
             {loading ? 'Uploading...' : 'Create Post'}
           </button>
