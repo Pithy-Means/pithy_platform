e770bd3b48fc4119d43deb71d3fe99a435e56cce
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-12-05 19:47:24 +0000

    Refactor Dashboard component: remove OverView wrapper for simplified layout

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,24 +1,17 @@
 import React from "react";
 import Communty from "@/components/communty";
-import OverView from "@/components/OverView";
 import PersonSidebar from "@/components/PersonSidebar";
 import ShareSomething from "@/components/ShareSomething";
 
 function Dashboard() {
   return (
-    <div className="">
-      <div className="bg-white/60">
-        <div className="flex space-x-4">
-          <OverView>
-            <div className="sticky top-0 h-screen">
-              <ShareSomething />
-            </div>
-            <div className="flex flex-col space-y-4 overflow-y-auto overflow-x-hidden w-1/4">
-              <PersonSidebar />
-              <Communty />
-            </div>
-          </OverView>
-        </div>
+    <div className="flex space-x-4">
+      <div className="sticky top-0 h-screen">
+        <ShareSomething />
+      </div>
+      <div className="flex flex-col space-y-4 overflow-y-auto overflow-x-hidden w-1/4">
+        <PersonSidebar />
+        <Communty />
       </div>
     </div>
   );
