b34d860c9262ed8722bd8e7f91a72835ca0efdca
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-12-05 19:47:04 +0000

    Refactor CourseCard component: update course props and improve navigation logic

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -1,34 +1,25 @@
 "use client";
-import React, { useEffect, useState } from "react";
+
 import Image from "next/image";
 import { MdAccessTimeFilled } from "react-icons/md";
 import { FaBookReader } from "react-icons/fa";
 import Link from "next/link";
-import Course from "@/types/Course";
 import { useRouter } from "next/navigation";
+import { Courses } from "@/types/schema";
 
 interface CourseCardProps {
-  courses: Course[];
+  courses: Courses[];
 }
 
 // const CourseCard = ({ courses }: CourseCardProps) => {}; //another way to define a functional component with props
 const CourseCard: React.FC<CourseCardProps> = ({ courses }) => {
   const router = useRouter();
-  const [isClient, setIsClient] = useState(false);
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    return null;
-  }
-
-  const handleViewMore = (_id: string | number) => {
+  const handleViewMore = (course_id: string | number) => {
     //check if router is available before navigating
-    if (isClient) {
+    if (router) {
       // Navigate to the course details page using the course id
-      router.push(`/singlecourse/${_id}`);
+      router.push(`/singlecourse/${course_id}`);
     } else {
       console.warn("Route is not available on the server side");
     }
@@ -36,10 +27,10 @@ const CourseCard: React.FC<CourseCardProps> = ({ courses }) => {
 
   return (
     <div className="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 gap-8 p-6 ">
-      {courses.map((course: Course) => (
-        <Link href={`/singlecourse/${course._id}`} key={course._id}>
+      {courses.map((course: Courses) => (
+        <Link href={`/singlecourse/${course.course_id}`} key={course.course_id}>
           <div
-            key={course._id}
+            key={course.course_id}
             className="bg-white shadow-lg rounded-lg overflow-hidden w-full"
           >
             <Image
@@ -47,6 +38,7 @@ const CourseCard: React.FC<CourseCardProps> = ({ courses }) => {
               alt={course.title}
               width={500}
               height={500}
+              unoptimized
               className="object-contain rounded-t-sm w-full "
             />
             <div className="p-4">
@@ -63,20 +55,20 @@ const CourseCard: React.FC<CourseCardProps> = ({ courses }) => {
                 </p>
                 <p className="text-gray-600 flex gap-2">
                   <FaBookReader size={20} className="text-orange-600 " />
-                  {course.learners} Learners
+                  {course.students} Learners
                 </p>
               </div>
               <div className="flex flex-row justify-between items-center p-2">
                 <p className=" text-green-600 text-base">
                   <del className="text-gray-400 pr-2 items-center flex flex-row">
-                    {course.originalPrice}
+                    {course.price}
                   </del>
                   Free
                 </p>
 
                 <button
                   type="button"
-                  onClick={() => handleViewMore(course._id)}
+                  onClick={() => handleViewMore(course.course_id)}
                   className="text-black/85 font-semibold text-base hover:border rounded-md  hover:bg-green-600/100 transition px-1 text-center "
                 >
                   View more
