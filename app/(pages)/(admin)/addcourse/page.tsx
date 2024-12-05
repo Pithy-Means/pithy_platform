dc32bafd3e78b309733d91e9bff4128424c9b7bd
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-12-05 19:44:17 +0000

    Refactor course creation page: replace CourseAdmin component with AddCourse component using CreateCourseForm

---
+++ app/(pages)/(admin)/addcourse/page.tsx
@@ -0,0 +1,11 @@
+import CreateCourseForm from "@/components/CreateCourseForm";
+
+const AddCourse = () => {
+  return (
+    <div>
+      <CreateCourseForm />
+    </div>
+  );
+};
+
+export default AddCourse;
