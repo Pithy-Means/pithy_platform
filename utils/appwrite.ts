8ebc6ec002d598a8cd5ece727621b6ad779ae452
Doreen Ikilai <dornkaizen@gmail.com>
2024-12-05 13:46:50 +0000

    Refactor Appwrite client session handling and streamline admin client creation

--- utils/appwrite.ts
+++ utils/appwrite.ts
@@ -9,7 +9,7 @@ export async function createSessionClient() {
     .setProject(env.appwrite.projectId);
     
 
-  const session = cookies().get("my-session");
+  const session = cookies().get("authToken");
   if (!session || !session.value) {
     throw new Error("No session");
   }
@@ -23,6 +23,32 @@ export async function createSessionClient() {
   };
 }
 
+// export const createAdminClient = async() => {
+//   const client = new Client();
+//   client
+//     .setEndpoint(env.appwrite.endpoint)
+//     .setProject(env.appwrite.projectId)
+//     .setKey(env.appwrite.apiKey);
+
+//     // console.log('Appwrite client created', client);
+  
+//   return {
+//     client: client,
+//     get account() {
+//       return new Account(client);
+//     },
+//     get databases() {
+//       return new Databases(client);
+//     },
+//     get users() {
+//       return new Users(client);
+//     },
+//     get storage() {
+//       return new Storage(client);
+//     },
+//   };
+// };
+
 export const createAdminClient = async() => {
   const client = new Client();
   client
@@ -31,19 +57,15 @@ export const createAdminClient = async() => {
     .setKey(env.appwrite.apiKey);
 
     // console.log('Appwrite client created', client);
+    const utils = {
+      account: new Account(client),
+      databases: new Databases(client),
+      users: new Users(client),
+      storage: new Storage(client),
+    };
   
-  return {
-    get account() {
-      return new Account(client);
-    },
-    get databases() {
-      return new Databases(client);
-    },
-    get users() {
-      return new Users(client);
-    },
-    get storage() {
-      return new Storage(client);
-    },
-  };
-};
+  return {client, ...utils};
+}
+
+// Explicitly export Account and other utilities if needed
+// export { Account, Databases, Users, Storage };
