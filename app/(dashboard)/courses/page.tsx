05483d09fcb28ab1294bd2a1e800b4d36fb3aaec
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-12-05 19:47:33 +0000

    Refactor Courses page: remove OverView wrapper for simplified structure

--- app/(dashboard)/courses/page.tsx
+++ app/(dashboard)/courses/page.tsx
@@ -1,17 +1,14 @@
-import OverView from "@/components/OverView";
-import CourseView from "@/components/courseView";
 import React from "react";
+import CourseView from "@/components/courseView";
 
 const page = () => {
   return (
     <div>
       <div className="bg-white/90">
         <div className="flex h-screen">
-          <OverView>
             <div className="w-full">
               <CourseView />
             </div>
-          </OverView>
         </div>
       </div>
     </div>
