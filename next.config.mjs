d52904114fae8097810b0a4937851e6b8005e927
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-12-09 08:45:20 +0000

    Update next.config.mjs: enhance cache configuration and add remote image patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,61 +1,71 @@
 /** @type {import('next').NextConfig} */
 
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-import withBundleAnalyzer from '@next/bundle-analyzer';
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 // Resolve __filename and __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const bundleAnalyzer = withBundleAnalyzer({
-    enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === "true",
 });
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-   
-    webpack: (config) => {
-        config.cache = {
-            type: 'filesystem',
-            buildDependencies: {
-                config: [resolve(__dirname, 'next.config.mjs')],
-            },
-            cacheDirectory: resolve(__dirname, '.next/cache/webpack'),
-        };
-        return config;
-    },
-    // Cache-Control Headers
-    async headers() {
-        return [
-            {
-                source: "/(.*).(js|css|woff|woff2|ttf|otf|eot|ico|jpg|jpeg|png|svg|gif|webp|avif)",
-                // source: "/:path*",
-                headers: [
-                    {
-                        key: "Cache-Control",
-                        value: "public, max-age=31536000, immutable",
-                    },
-                ],
-            },
-            {
-                source: "/_next/static/(.*)",
-                headers: [
-                    {
-                        key: "Cache-Control",
-                        value: "public, max-age=31536000, immutable",
-                    },
-                ],
-            },
-        ];
-    },
+  reactStrictMode: true,
+  swcMinify: true,
 
-    // // Incremental Static Regeneration not configured globally but per page basis
-    // async revalidate() {
-    //     return {
-    //         revalidate: 60, // Revalidate pages every 60 seconds
-    //     };
-    // },
+  webpack: (config) => {
+    config.cache = {
+      type: "filesystem",
+      buildDependencies: {
+        config: [resolve(__dirname, "next.config.mjs")],
+      },
+      cacheDirectory: resolve(__dirname, ".next/cache/webpack"),
+    };
+    return config;
+  },
+  // Cache-Control Headers
+  async headers() {
+    return [
+      {
+        source:
+          "/(.*).(js|css|woff|woff2|ttf|otf|eot|ico|jpg|jpeg|png|svg|gif|webp|avif)",
+        // source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/_next/static/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'api.microlink.io',
+          pathname: '/**', // This allows images from any path on this domain
+        },
+      ],
+  },
+
+  // // Incremental Static Regeneration not configured globally but per page basis
+  // async revalidate() {
+  //     return {
+  //         revalidate: 60, // Revalidate pages every 60 seconds
+  //     };
+  // },
 };
 
 export default bundleAnalyzer(nextConfig);
