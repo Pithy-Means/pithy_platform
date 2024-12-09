077e05c447e7ebf0b1cf95c0ea28fb5c25a12d8e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-12-09 08:51:06 +0000

    Refactor page component: streamline JSX structure for improved readability

--- app/(dashboard)/courses/page.tsx
+++ app/(dashboard)/courses/page.tsx
@@ -3,13 +3,9 @@ import CourseView from "@/components/courseView";
 
 const page = () => {
   return (
-    <div>
-      <div className="bg-white/90">
-        <div className="flex h-screen">
-            <div className="w-full">
-              <CourseView />
-            </div>
-        </div>
+    <div className="flex h-screen">
+      <div className="w-full">
+        <CourseView />
       </div>
     </div>
   );
