1ecda3ca481f87869eaefa63bf53087cac5474c5
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-12-09 08:53:14 +0000

    Refactor ShareSomething component: improve layout and button styling for post creation

--- components/ShareSomething.tsx
+++ components/ShareSomething.tsx
@@ -12,21 +12,23 @@ const ShareSomething = () => {
     <div className="flex flex-col w-full no-scrollbar max-h-screen">
       {/* Header Section */}
       <div className="flex flex-col bg-white text-black px-6 justify-center rounded h-16 p-4 m-6">
-        <div className="flex flex-row items-center gap-4">
-          <CircleUserRound size={32} />
-          {/* Add the functionality of create a post */}
-          <InputContact
-            type="text"
-            label="Share something"
-            className="border-2 border-gray-300 rounded-lg w-full"
-            value="" // Add the value property
-            onChange={() => {}} // Open modal when the input is clicked
-          />
-          <button
-            className="bg-gradient-to-t from-[#5AC35A] to-[#00AE76] text-white rounded-lg p-2 w-20"
-          >
-            Post
-          </button>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-4 w-3/4">
+            <CircleUserRound size={32} />
+            {/* Add the functionality of create a post */}
+            <InputContact
+              type="text"
+              label="Share something"
+              className="border-2 border-gray-300 rounded-lg w-full"
+              value="" // Add the value property
+              onChange={() => {}} // Open modal when the input is clicked
+            />
+          </div>
+            <button
+              className="bg-gradient-to-t from-[#5AC35A] to-[#00AE76] text-white rounded-lg py-2 px-6"
+            >
+              Post
+            </button>
         </div>
       </div>
       {/* Scrollable Posts Section */}
