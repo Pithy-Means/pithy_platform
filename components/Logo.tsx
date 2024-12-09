7709c00629e3dcd76b86f6168ca0fea3e4e929fd
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-12-09 19:04:45 +0000

    Update Logo component: enhance layout with logo image and title styling

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <div className="flex items-center">
+      <img src="assets/logo.png" alt="Logo" className="w-8 h-8" />
+      <span className="text-xl font-bold ml-2">Pithy Means</span>
+    </div>
+  );
 };
 
 export default Logo;
+
