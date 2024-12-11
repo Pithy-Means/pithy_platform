bf349d534f5216fbe05f7145a5a3b2ef7cd5756b
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-12-11 07:16:17 +0000

    Add optional width and height props to Video component for better customization

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,12 +1,14 @@
 interface Video {
   src: string;
   controls: boolean;
+  width?: string;
+  height?: string;
   className?: string;
 }
 
-export function Video({src, controls, className}: Video) {
+export function Video({src, controls, width, height, className}: Video) {
   return (
-    <video width="320" height="240" className={className} controls={controls} preload="none">
+    <video width={width} height={height} className={className} controls={controls} preload="none">
       <source src={src} type="video/mp4" />
     </video>
   )
