38929b81bb67766fa5b2c05cfdbe251b1f2a0fe8
Doreen Ikilai <dornkaizen@gmail.com>
2024-12-13 16:31:29 +0000

    Add optional className prop to TitleDot component for custom styling

--- components/TitleDot.tsx
+++ components/TitleDot.tsx
@@ -1,11 +1,12 @@
 interface TitleDotProps {
   title: string;
+  className?: string;
 }
 
-const TitleDot: React.FC<TitleDotProps> = ({ title }) => {
+const TitleDot: React.FC<TitleDotProps> = ({ title, className }) => {
   return (
     <div className="flex items-center justify-center md:justify-center lg:justify-start space-x-2 w-full">
-      <h1 className="text-xl sm:text-[6px] md:text-base lg:text-3xl md:text-center lg:text-start text-center text-black font-extrabold capitalize">
+      <h1 className={`text-xl sm:text-[6px] md:text-base lg:text-3xl md:text-center lg:text-start text-center text-black font-extrabold capitalize ${className}`}>
         {title}
       </h1>
       <span className="bg-gradient-to-t from-[#5AC35A] to-[#00AE76] h-1.5 w-1.5 rounded-full"></span>
