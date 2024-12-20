f8e935ab02927b50defe9d5cd6a04f4bf80653ca
Ikilia Doreen <ikilaidoreen@gmail.com>
2024-12-20 16:39:58 +0300

    Enhance Header_description component: adjust text sizes for improved responsiveness

--- components/Header_description.tsx
+++ components/Header_description.tsx
@@ -9,12 +9,12 @@ const Header_description = () => {
         Unlock Your Dream Career.
       </h1>
       {/* Responsive paragraph */}
-      <p className="text-base sm:text-lg lg:text-start text-center">
+      <p className="text-base sm:text-lg md:text-xl lg:text-start text-center">
         Discover Your Strengths, Interest, Perfect Career Match And The Business
         Venture For You
       </p>
       {/* Responsive button */}
-      <Button className="bg-gradient-to-tr from-[#5AC35A] to-[#00AE76] w-fit text-sm sm:text-base px-4 py-2">
+      <Button className="bg-gradient-to-tr from-[#5AC35A] to-[#00AE76] w-fit text-sm sm:text-base md:text-2xl px-4 py-2">
         Start Your Journey
       </Button>
     </div>
