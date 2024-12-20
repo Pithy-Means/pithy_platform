9c1f228d022a301f9fae5e72618082995a741be8
Ikilia Doreen <ikilaidoreen@gmail.com>
2024-12-20 16:36:30 +0300

    Enhance Pricing component: improve text sizes for better responsiveness

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -7,8 +7,8 @@ const Pricing = () => {
   return (
     <div className="bg-white px-10 pb-4">
       <div className="flex flex-col space-y-4 lg:items-start items-center mb-6">
-        <TitleDot title={"Pricing"} />
-        <p className="capitalize text-black">One time payment</p>
+        <TitleDot title={"Pricing"} className="md:text-3xl md:pt-4"/>
+        <p className="capitalize text-black md:text-xl">One time payment</p>
       </div>
       <Card className="bg-white flex flex-col lg:flex-row md:space-x-8 space-y-8 lg:space-y-0 p-6 lg:p-10 items-center border border-black/10 rounded-md drop-shadow-2xl shadow lg:w-full w-fit mx-auto">
         <div className="bg-[#37BB65] rounded-lg">
@@ -23,24 +23,24 @@ const Pricing = () => {
           />
         </div>
         <div className="flex flex-col space-y-4 w-full lg:items-start items-center md:w-auto text-center md:text-left">
-          <div className="text-black font-extrabold capitalize">
+          <div className="text-black font-extrabold capitalize md:text-xl">
             Only For you
           </div>
           <div className="flex flex-col space-y-4">
             <div className="flex flex-col lg:flex-row space-y-2 lg:space-x-2 items-center">
-              <div className="border-2 border-[#5AC35A] rotate-45 h-1 w-1 p-0.5"></div>
-              <p className="capitalize text-black">
+              <div className="border-2 border-[#5AC35A] rotate-45 h-1 w-1 p-0.5 md:h-2 md:w-4 md:p-1.5"></div>
+              <p className="capitalize text-black md:text-lg">
                 For <b>$5</b> or UGX20,000, get 3 months access to the online
                 orientation.
               </p>
             </div>
             <div className="flex flex-col lg:flex-row space-y-2 lg:space-x-2 items-center">
-              <div className="border-2 border-[#5AC35A] rotate-45 h-1 w-1 p-0.5"></div>
-              <p className="capitalize text-black">
+              <div className="border-2 border-[#5AC35A] rotate-45 h-1 w-1 p-0.5 md:h-2 md:w-4 md:p-1.5"></div>
+              <p className="capitalize text-black md:text-lg">
                 Become an affiliate and earn 10% referral fees.
               </p>
             </div>
-            <Button className="text-white bg-gradient-to-r from-[#5AC35A] to-[#00AE76] lg:mx-0 mx-auto w-fit">
+            <Button className="text-white md:text-xl bg-gradient-to-r from-[#5AC35A] to-[#00AE76] lg:mx-0 mx-auto w-fit">
               Get Started
             </Button>
           </div>
