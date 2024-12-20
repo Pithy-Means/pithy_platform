13169e17d0e6c51f89813a26af8e2601fc244ae8
Ikilia Doreen <ikilaidoreen@gmail.com>
2024-12-20 19:55:11 +0300

    Enhance AssCards component: improve responsiveness and adjust text sizes for better readability

--- components/AssCards.tsx
+++ components/AssCards.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 
 const AssCards = () => {
   return (
-    <div className="flex flex-col lg:flex-row space-y-6 items-center xl:space-y-8 4k:space-y-12 ">
+    <div className="flex flex-col lg:flex-row space-y-6 items-center lg:space-y-0 lg:space-x-6 xl:space-y-8 4k:space-y-12 ">
       <div className="flex  items-center ">
-        <div className="flex flex-col items-center lg:gap-0 space-y-2 lg:mb-20 mb-0 xl:mb-72 4k:mb-96">
+        <div className="flex flex-col items-center space-y-2 lg:space-y-6 xl:space-y-8 4k:space-y-12 ">
           <div className="text-center block lg:hidden">
             <div className="h-6 w-6 bg-gradient-to-r from-[#5AC35A] to-[#00AE76] rounded-full p-4 text-white flex justify-center items-center text-center">
               1
@@ -14,12 +14,12 @@ const AssCards = () => {
           </div>
           <Card className="bg-white w-64 md:w-96 lg:w-72 xl:w-[28rem] 4k:w-[36rem] ">
             <div className="bg-gradient-to-r from-[#5AC35A] to-[#00AE76] rounded-t-sm px-4 py-2">
-              <h3 className="text-lg md:text-xl xl:text-4xl 4k:text-2xl font-extrabold text-black capitalize">
+              <h3 className="text-lg md:text-xl xl:text-2xl 4k:text-3xl font-extrabold text-black capitalize">
                 Take our assessment
               </h3>
             </div>
             <div className="flex items-center flex-col space-y-2 py-4">
-              <p className="capitalize text-black md:text-xl ">
+              <p className="capitalize text-black md:text-xl lg:text-2xl xl:text-3xl 4k:text-4xl">
                 Answer a series of questions
               </p>
               <Image
@@ -60,18 +60,18 @@ const AssCards = () => {
           alt="Join"
           width={150}
           height={100}
-          className="mb-[300px]  lg:block hidden"
+          className="mb-[300px]  lg:block hidden lg:mb-[-10px]"
         />
       </div>
-      <div className="flex flex-col items-center lg:gap-0  space-y-2 lg:space-y-0 xl:gap-4 4k:gap-6">
-        <Card className="bg-white w-64 md:w-96 xl:w-[28rem] 4k:w-[36rem] ml-0 lg:ml-2 lg:mt-[-240px] ">
+      <div className="flex flex-col items-center space-y-2 lg:space-y-6 xl:space-y-8 4k:space-y-12">
+        <Card className="bg-white w-64 md:w-96 xl:w-[28rem] 4k:w-[36rem]">
           <div className="bg-gradient-to-r from-[#5AC35A] to-[#00AE76] rounded-t-sm px-4 py-2">
-            <h3 className="text-lg md:text-xl font-extrabold text-black capitalize first-line:">
+            <h3 className="text-lg md:text-xl xl:text-2xl 4k:text-3xl font-extrabold text-black capitalize first-line:">
               Get personalized results
             </h3>
           </div>
           <div className="flex items-center flex-col space-y-2 py-4">
-            <p className="capitalize text-black md:text-xl">receive a customized report</p>
+            <p className="capitalize text-black md:text-xl lg:text-2xl xl:text-3xl 4k:text-4xl">receive a customized report</p>
             <Image src="/assets/Frame 88.png"
               alt="Frame"
               width={60}
@@ -106,15 +106,15 @@ const AssCards = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center ">
-        <Card className="bg-white w-64 md:w-96 xl:w-[28rem] 4k:w-[36rem] lg:ml-28 lg:mt-[-140px]">
+      <div className="flex flex-col items-center space-y-2 lg:space-y-6 xl:space-y-8 4k:space-y-12">
+        <Card className="bg-white w-64 md:w-96 xl:w-[28rem] 4k:w-[36rem]">
           <div className="bg-gradient-to-r from-[#5AC35A] to-[#00AE76] rounded-t-sm px-4 py-2">
-            <h3 className="text-lg md:text-xl font-extrabold text-black capitalize">
+            <h3 className="text-lg md:text-xl xl:text-2xl 4k:text-3xl font-extrabold text-black capitalize">
               Explore and take action
             </h3>
           </div>
-          <div className="flex items-center flex-col space-y-2 py-4 px-6">
-            <p className="capitalize text-black md:text-xl">
+          <div className="flex items-center flex-col space-y-2 py-4">
+            <p className="capitalize text-black md:text-xl lg:text-2xl xl:text-3xl 4k:text-4xl">
               review your results & explore recommendations
             </p>
             <Image src="/assets/Frame 89.png"
