2a3f04c5b7cc9318060c950f30158dd2b4f77fe0
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-12-21 04:25:06 +0000

    Remove unnecessary permissions from post attachment bucket setup

--- models/server/storageSetup.ts
+++ models/server/storageSetup.ts
@@ -1,4 +1,3 @@
-import { Permission, Role } from "node-appwrite";
 import { postAttachementBucket } from "../name";
 import { storage } from "./config";
 
@@ -12,16 +11,7 @@ export default async function setupStorage() {
       await storage.createBucket(
         postAttachementBucket,
         postAttachementBucket,
-        [
-          Permission.read(Role.users()), // Everyone can read
-          Permission.create(Role.users()), // Everyone can write
-          Permission.update(Role.users()), // Everyone can update
-          Permission.delete(Role.users()), // Everyone can delete
-          Permission.read(Role.team("admin")), // Admins can read
-          Permission.create(Role.team("admin")), // Admins can write
-          Permission.update(Role.team("admin")), // Admins can update
-          Permission.delete(Role.team("admin")), // Admins can delete
-        ],
+        [],
         false,
         true,
         50000000,
