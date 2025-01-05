2b868e70657bd4e25d7313ecc4cbc77085c004b5
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-01-05 18:59:49 +0000

    Refactor PersonSidebar component layout for improved spacing and typography

--- components/PersonSidebar.tsx
+++ components/PersonSidebar.tsx
@@ -21,9 +21,9 @@ const PersonSidebar = () => {
   const { user } = useContext(UserContext);
 
   return (
-    <div className="flex flex-col bg-white w-full text-black justify-start h-[calc(100vh-64px)] overflow-y-auto mt-6 mr-4 rounded-lg overflow-hidden">
+    <div className="flex flex-col bg-white w-full text-black justify-start overflow-y-auto mt-6 mr-4 rounded-lg overflow-hidden">
       <div className="bg-green-500 h-16 rounded-t-lg w-full"></div>
-      <div className="flex flex-col items-center justify-center gap-4 bg-white">
+      <div className="flex flex-col items-center justify-center space-y-2 bg-white">
         {user && (
           <div className="flex flex-col justify-center items-center -mt-7">
             <div className="bg-white rounded-full p-2">
@@ -32,7 +32,7 @@ const PersonSidebar = () => {
               </p>
             </div>
             <div className="text-black items-center flex flex-col space-y-1">
-              <span className="text-lg">
+              <span className="text-lg font-bold">
                 {user.firstname} {user.lastname}
               </span>
               <button className="text-base bg-gray-400 rounded-sm px-2 py-1">
