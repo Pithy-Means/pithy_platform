3a9a9b69a4985ab7b498ddfe37cf7f01f024e31c
Doreen Ikilai <dornkaizen@gmail.com>
2025-01-06 17:14:00 +0000

    Enhance responsiveness of Howdoesitwork component with improved text size adjustments

--- components/Howdoesitwork.tsx
+++ components/Howdoesitwork.tsx
@@ -6,7 +6,7 @@ const Howdoesitwork = () => {
     <div className="px-10 py-4 bg-white">
       <div className="flex flex-col lg:space-y-2 space-y-8 lg:items-start items-center">
         <TitleDot title={"how does it work?"}  className="lg: text-xl md:text-lg "/>
-        <p className="capitalize text-black lg:text-start text-center">
+        <p className="capitalize text-black lg:text-start text-center text-base">
           Discover your ideal career path in 3 easy Steps
         </p>
         <AssCards />
