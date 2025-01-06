9092bc7819f6d2659dfe3efee27e3279c5c86c3a
Ikilia Doreen <ikilaidoreen@gmail.com>
2025-01-06 09:16:11 +0300

    Enhance AboutPage and Header_description components: improve responsiveness with additional text size adjustments for larger screens

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
   return (
     <div className="flex flex-col items-center w-full">
       <div className="flex justify-center items-center h-72 bg-black w-full top-0">
-        <h1 className="text-green-600 text-lg">About Us</h1>
+        <h1 className="text-green-600 text-lg md:text-2xl lg:text-3xl xl:text-5xl 4k:text-6xl">About Us</h1>
       </div>
       <OurStory />
       <Partnerships />
