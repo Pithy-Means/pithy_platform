e6958ff2aa3ea39fd1a344abe704033738ac3668
Doreen Ikilai <dornkaizen@gmail.com>
2025-01-06 11:45:08 +0000

    Enhance responsiveness of CardImage component with additional sizes for larger screens

--- components/CardImage.tsx
+++ components/CardImage.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const CardImage = () => {
   return (
-    <div className="bg-gradient-to-b from-[#5AC35A] via-[#00AE76] to-[#000] rounded-r-3xl flex items-center justify-center h-[380px] w-[350px] lg:p-0 px-10">
+    <div className="bg-gradient-to-b from-[#5AC35A] via-[#00AE76] to-[#000] rounded-r-3xl flex items-center justify-center h-[380px] w-[350px] lg:p-0 px-10 3xl:w-[450px] 3xl:h-[650px]">
       <Image
         src="/assets/Standing_ladies.png"
         height={450}
