8e7529f99cf8116deb2fd5ea36206637311f5e74
Ikilia Doreen <ikilaidoreen@gmail.com>
2025-01-06 09:19:04 +0300

    Enhance Howdoesitwork component: adjust text sizes for improved readability on larger screens

--- components/Howdoesitwork.tsx
+++ components/Howdoesitwork.tsx
@@ -6,7 +6,7 @@ const Howdoesitwork = () => {
     <div className="px-10 py-4 bg-white">
       <div className="flex flex-col lg:space-y-2 space-y-8 lg:items-start items-center">
         <TitleDot title={"how does it work?"}  className="lg:text-xl md:text-3xl "/>
-        <p className="capitalize text-black lg:text-start text-center md:text-xl">
+        <p className="capitalize text-black text-base lg:text-start text-center md:text-lg lg:text-lg">
           Discover your ideal career path in 3 easy Steps
         </p>
         <AssCards />
