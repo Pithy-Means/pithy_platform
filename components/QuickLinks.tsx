262dcf682fe5a6814a2d147a4fbc4fd8fd1dae18
Doreen Ikilai <dornkaizen@gmail.com>
2025-01-06 18:08:31 +0000

    Enhance responsiveness of QuickLinks component with improved text colors and hover effects

--- components/QuickLinks.tsx
+++ components/QuickLinks.tsx
@@ -4,42 +4,42 @@ const QuickLinks: React.FC = () => {
   return (
     <div className="flex flex-col space-y-4 md:w-1/5 text-center justify-center w-full ">
       <h3 className="text-lg font-bold text-white">Quick Links</h3>
-      <nav className="flex flex-col space-y-2">
+      <nav className="flex flex-col space-y-2 ">
         <LinkPreview
           url="https://pithy-means.netlify.app/"
-          className="text-[12px] md:text-sm lg:text-base text-gray-600 hover:text-white transition duration-300 ease-in-out"
+          className="text-[12px] md:text-sm lg:text-base text-white hover:text-green-600 transition duration-300 ease-in-out"
         >
           Home
         </LinkPreview>
         <LinkPreview
           url="https://pithy-means.netlify.app/about"
-          className="text-[12px] md:text-sm lg:text-base text-gray-600 hover:text-white transition duration-300 ease-in-out"
+          className="text-[12px] md:text-sm lg:text-base text-white hover:text-green-600transition duration-300 ease-in-out"
         >
           About
         </LinkPreview>
         <LinkPreview
           url="https://pithy-means.netlify.app/how-it-works"
-          className="text-[12px] md:text-sm lg:text-base text-gray-600 hover:text-white transition duration-300 ease-in-out"
+          className="text-[12px] md:text-sm lg:text-base text-white hover:text-green-600 transition duration-300 ease-in-out"
         >
           How It Works
         </LinkPreview>
         <LinkPreview
           url="https://pithy-means.netlify.app/contact"
-          className="text-[12px] md:text-sm lg:text-base text-gray-600 hover:text-white transition duration-300 ease-in-out"
+          className="text-[12px] md:text-sm lg:text-base text-white hover:text-green-600 transition duration-300 ease-in-out"
         >
           Contact
         </LinkPreview>
         <div className="flex lg:space-x-2 text-center justify-center flex-wrap lg:flex-row flex-col">
           <LinkPreview
             url="https://pithy-means.netlify.app/signIn"
-            className="text-[12px] md:text-sm lg:text-base text-gray-600 hover:text-white transition duration-300 ease-in-out"
+            className="text-[12px] md:text-sm lg:text-base text-white hover:text-green-600 transition duration-300 ease-in-out"
           >
             Login
           </LinkPreview>
           <span className="text-gray-600 hidden lg:block">|</span>
           <LinkPreview
             url="https://pithy-means.netlify.app/signUp"
-            className="text-[12px] md:text-sm lg:text-base text-gray-600 hover:text-white transition duration-300 ease-in-out"
+            className="text-[12px] md:text-sm lg:text-base text-white hover:text-green-600transition duration-300 ease-in-out"
           >
             Sign Up
           </LinkPreview>
