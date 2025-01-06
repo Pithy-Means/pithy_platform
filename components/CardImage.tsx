7524090db9aef75a6e04ecee202e3bf95f4e5f11
Ikilia Doreen <ikilaidoreen@gmail.com>
2025-01-06 09:16:59 +0300

    Enhance CardImage component: adjust image dimensions for improved layout on larger screens

--- components/CardImage.tsx
+++ components/CardImage.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const CardImage = () => {
   return (
-    <div className="bg-gradient-to-b from-[#5AC35A] via-[#00AE76] to-[#000] rounded-r-3xl flex items-center justify-center h-[380px] w-[350px] lg:p-0 px-10 lg:h-[480px] lg:w-[400px] xl:h-[600px] xl:w-[500px] 4k:h-[800px] 4k:w-[700px]">
+    <div className="bg-gradient-to-b from-[#5AC35A] via-[#00AE76] to-[#000] rounded-r-3xl flex items-center justify-center h-[380px] w-[350px] lg:p-0 px-10 lg:h-[480px] lg:w-[400px] xl:h-[500px] xl:w-[500px] 4k:h-[800px] 4k:w-[700px]">
       <Image
         src="/assets/Standing_ladies.png"
         height={450}
