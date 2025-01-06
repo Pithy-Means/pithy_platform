c1cd6e41733887b66e32c9afb3b347f128a68401
Doreen Ikilai <dornkaizen@gmail.com>
2025-01-06 17:16:07 +0000

    Enhance responsiveness of WhoWeAreMobile component with improved text sizes

--- components/WhoWeAreMobile.tsx
+++ components/WhoWeAreMobile.tsx
@@ -8,10 +8,10 @@ const WhoWeAreMobile = () => {
     <div className="flex flex-col space-y-10 items-center bg-white p-10">
       <div className="flex flex-col space-y-4 items-center">
         <TitleDot title={"Who We Are"} />
-        <p className="capitalize text-black text-center">
+        <p className="capitalize text-black text-center text-base">
           Empowering informed decisions for a fulfing career
         </p>
-        <p className="text-black text-center">
+        <p className="text-black text-center text-base  ">
           At pithy means, we believe that everyone deserves to find their
           perfect fit. our mission is to guide students, professionals, and
           business managers towards their most suitable study, professional, or
@@ -26,31 +26,31 @@ const WhoWeAreMobile = () => {
         <div className="bg-[#5AC35A] w-fit text-black font-extrabold px-2 rounded-md">
           Our Story
         </div>
-        <p className="capitalize text-black text-center">
+        <p className="capitalize text-black text-center text-base">
           Founded in the US and Uganda, our team saw the need to prevent:
         </p>
         <div className="flex flex-col space-y-4">
           <div className="flex space-y-2 flex-col items-center">
             <div className="border-2 border-[#5AC35A] rotate-45 h-1 w-1 p-0.5"></div>
-            <p className="capitalize text-black text-center">
+            <p className="capitalize text-black text-center text-base">
               identify natural strengths and interests
             </p>
           </div>
           <div className="flex space-y-2 flex-col items-center">
             <div className="border-2 border-[#5AC35A] rotate-45 p-0.5 h-1 w-1"></div>
-            <p className="capitalize text-black text-center">
+            <p className="capitalize text-black text-base text-center">
               Explore suitable career and business paths
             </p>
           </div>
           <div className="flex space-y-2 flex-col items-center">
             <div className="border-2 border-[#5AC35A] rotate-45 p-0.5 h-1 w-1"></div>
-            <p className="capitalize text-black">Make informed decisions</p>
+            <p className="capitalize text-black text-base text-center">Make informed decisions</p>
           </div>
         </div>
         <div className="bg-[#5AC35A] w-fit text-black font-extrabold px-2 rounded-md">
           Our vision
         </div>
-        <p className="capitalize text-black text-center">
+        <p className="capitalize text-black text-base text-center ">
           A world where everyone thrives in their chosen path.
         </p>
         <Button className="text-white bg-gradient-to-r from-[#5AC35A] to-[#00AE76] w-fit">
