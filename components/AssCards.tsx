8a16550c718c003573f8c987c5e6a1cc07363096
Ikilia Doreen <ikilaidoreen@gmail.com>
2025-01-06 09:16:49 +0300

    Enhance AssCards component: improve responsiveness and adjust text sizes for better readability on larger screens

--- components/AssCards.tsx
+++ components/AssCards.tsx
@@ -6,20 +6,20 @@ const AssCards = () => {
   return (
     <div className="flex flex-col lg:flex-row space-y-6 items-center lg:space-y-0 lg:space-x-6 xl:space-y-8 4k:space-y-12 ">
       <div className="flex  items-center ">
-        <div className="flex flex-col items-center space-y-2 lg:space-y-6 xl:space-y-8 4k:space-y-12 ">
+        <div className="flex flex-col items-center space-y-2 lg:space-y-6 xl:space-y-8 4k:space-y-12 4k:space-x-20  ">
           <div className="text-center block lg:hidden">
             <div className="h-6 w-6 bg-gradient-to-r from-[#5AC35A] to-[#00AE76] rounded-full p-4 text-white flex justify-center items-center text-center">
               1
             </div>
           </div>
-          <Card className="bg-white w-64 md:w-96 lg:w-72 xl:w-[28rem] 4k:w-[36rem] ">
+          <Card className="bg-white w-64 md:w-72 lg:w-72 xl:w-72 4k:w-[32rem] 4k:relative 4k:-right-20 ">
             <div className="bg-gradient-to-r from-[#5AC35A] to-[#00AE76] rounded-t-sm px-4 py-2">
               <h3 className="text-lg md:text-xl xl:text-2xl 4k:text-3xl font-extrabold text-black capitalize">
                 Take our assessment
               </h3>
             </div>
             <div className="flex items-center flex-col space-y-2 py-4">
-              <p className="capitalize text-black md:text-xl lg:text-2xl xl:text-3xl 4k:text-4xl">
+              <p className="capitalize text-black md:text-lg lg:text-xl xl:text-lg 4k:text-4xl">
                 Answer a series of questions
               </p>
               <Image
@@ -60,18 +60,18 @@ const AssCards = () => {
           alt="Join"
           width={150}
           height={100}
-          className="mb-[300px]  lg:block hidden lg:mb-[-10px]"
+          className="mb-[300px] md:mt-20 lg:block hidden lg:mb-[-10px] 4k:relative 4k:-right-20"
         />
       </div>
       <div className="flex flex-col items-center space-y-2 lg:space-y-6 xl:space-y-8 4k:space-y-12">
-        <Card className="bg-white w-64 md:w-96 xl:w-[28rem] 4k:w-[36rem]">
+        <Card className="bg-white w-64 md:w-72  xl:w-72 4k:w-[32rem] 4k:-mt-32 4k:-right-20 4k:relative 4k:mt-40">
           <div className="bg-gradient-to-r from-[#5AC35A] to-[#00AE76] rounded-t-sm px-4 py-2">
             <h3 className="text-lg md:text-xl xl:text-2xl 4k:text-3xl font-extrabold text-black capitalize first-line:">
               Get personalized results
             </h3>
           </div>
           <div className="flex items-center flex-col space-y-2 py-4">
-            <p className="capitalize text-black md:text-xl lg:text-2xl xl:text-3xl 4k:text-4xl">receive a customized report</p>
+            <p className="capitalize text-black md:text-xl lg:text-xl xl:text-lg 4k:text-4xl">receive a customized report</p>
             <Image src="/assets/Frame 88.png"
               alt="Frame"
               width={60}
@@ -88,16 +88,16 @@ const AssCards = () => {
             alt=""
             width={150}
             height={100}
-            className=" hidden lg:block relative -right-40 top-1/2 transform -translate-y-1/2 rotate-12 mt-[-10px]"
+            className=" hidden lg:block relative -right-40 top-1/2 transform -translate-y-1/2 rotate-12 mt-[-32px] 4k:block 4k:-right-[24rem]"
           />
         </div>
         <div className="block lg:hidden">
           <Image src="/assets/green.png"
             alt="Take"
-            width={50}
-            height={50}
+            width={20}
+            height={100}
             priority
-            className="mb-[300px]  lg:block hidden"
+            className=" lg:hidden block"
           />
         </div>
         <div className="text-center block lg:hidden">
@@ -107,14 +107,14 @@ const AssCards = () => {
         </div>
       </div>
       <div className="flex flex-col items-center space-y-2 lg:space-y-6 xl:space-y-8 4k:space-y-12">
-        <Card className="bg-white w-64 md:w-96 xl:w-[28rem] 4k:w-[36rem]">
+        <Card className="bg-white w-64 md:w-972 xl:w-72 4k:w-[32rem] xl:ml-[6rem] 4k:relative 4k:-mt-18 4k:-right-32">
           <div className="bg-gradient-to-r from-[#5AC35A] to-[#00AE76] rounded-t-sm px-4 py-2">
             <h3 className="text-lg md:text-xl xl:text-2xl 4k:text-3xl font-extrabold text-black capitalize">
               Explore and take action
             </h3>
           </div>
           <div className="flex items-center flex-col space-y-2 py-4">
-            <p className="capitalize text-black md:text-xl lg:text-2xl xl:text-3xl 4k:text-4xl">
+            <p className="capitalize text-black md:text-xl lg:text-2xl xl:text-lg xl:px-2 4k:text-4xl">
               review your results & explore recommendations
             </p>
             <Image src="/assets/Frame 89.png"
