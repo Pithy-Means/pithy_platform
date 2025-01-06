d4ac71f85c9b0c4764711be4f7fb5a4a83a7723d
Doreen Ikilai <dornkaizen@gmail.com>
2025-01-06 17:14:29 +0000

    Enhance responsiveness of Pricing component with improved text sizes

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -8,7 +8,7 @@ const Pricing = () => {
     <div className="bg-white px-10 pb-4">
       <div className="flex flex-col space-y-4 lg:items-start items-center mb-6">
         <TitleDot title={"Pricing"} />
-        <p className="capitalize text-black">One time payment</p>
+        <p className="capitalize text-black text-sm sm:text-base">One time payment</p>
       </div>
       <Card className="bg-white flex flex-col lg:flex-row md:space-x-8 space-y-8 lg:space-y-0 p-6 lg:p-10 items-center border border-black/10 rounded-md drop-shadow-2xl shadow lg:w-full w-fit mx-auto">
         <div className="bg-[#37BB65] rounded-lg">
@@ -29,14 +29,14 @@ const Pricing = () => {
           <div className="flex flex-col space-y-4">
             <div className="flex flex-col lg:flex-row space-y-2 lg:space-x-2 items-center">
               <div className="border-2 border-[#5AC35A] rotate-45 h-1 w-1 p-0.5"></div>
-              <p className="capitalize text-black">
+              <p className="capitalize text-black text-sm sm:text-base">
                 For <b>$5</b> or UGX20,000, get 3 months access to the online
                 orientation.
               </p>
             </div>
             <div className="flex flex-col lg:flex-row space-y-2 lg:space-x-2 items-center">
               <div className="border-2 border-[#5AC35A] rotate-45 h-1 w-1 p-0.5"></div>
-              <p className="capitalize text-black">
+              <p className="capitalize text-black text-sm sm:text-base">
                 Become an affiliate and earn 10% referral fees.
               </p>
             </div>
