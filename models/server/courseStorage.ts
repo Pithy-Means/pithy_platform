e2ed15d046e5b21f296731da7a6d6fd02337591a
Doreen Ikilai <dornkaizen@gmail.com>
2025-01-07 10:22:13 +0000

    Remove unnecessary permissions from course attachment bucket creation for simplification

--- models/server/courseStorage.ts
+++ models/server/courseStorage.ts
@@ -1,4 +1,3 @@
-import { Permission, Role } from "node-appwrite";
 import { courseAttachementBucket } from "../name";
 import { storage } from "./config";
 
@@ -12,16 +11,7 @@ export default async function courseStorage() {
       await storage.createBucket(
         courseAttachementBucket,
         courseAttachementBucket,
-        [
-          Permission.read(Role.users()), // Everyone can read
-          Permission.create(Role.users()), // Everyone can write
-          Permission.update(Role.users()), // Everyone can update
-          Permission.delete(Role.users()), // Everyone can delete
-          Permission.read(Role.team("admin")), // Admins can read
-          Permission.create(Role.team("admin")), // Admins can write
-          Permission.update(Role.team("admin")), // Admins can update
-          Permission.delete(Role.team("admin")), // Admins can delete
-        ],
+        [],
         false,
         true,
         50000000,
