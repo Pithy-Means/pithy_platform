ecf65b2b4788d94a3e89037cce51f74a05843acd
Doreen Ikilai <dornkaizen@gmail.com>
2025-01-07 09:42:39 +0000

    Add CourseView component to dashboard courses page

---
+++ app/(dashboard)/dashboard/courses/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CourseView from "@/components/courseView";
+
+const page = () => {
+  return (
+    <div className="flex h-screen">
+      <div className="w-full">
+        <CourseView />
+      </div>
+    </div>
+  );
+};
+
+export default page;
