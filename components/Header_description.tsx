32df66212b59539c4b089285d1a2ee0c746197ed
Doreen Ikilai <dornkaizen@gmail.com>
2025-01-07 09:50:48 +0000

    Update Header_description component for improved responsiveness and enhanced button text size

--- components/Header_description.tsx
+++ components/Header_description.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 const Header_description = () => {
   return (
-    <div className="flex flex-col lg:items-start items-center space-y-4 w-full px-4 lg:px-0">
+    <div className="flex flex-col lg:items-start xl:items-start items-center space-y-4 w-full px-4 lg:px-0 xl:px-2 4k:px-6">
       {/* Reduced and responsive heading */}
       <h1 className="text-xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-5xl 3xl:text-text-6xl
        text-[#5AC35A] font-extrabold lg:text-start text-center">
@@ -15,7 +15,7 @@ const Header_description = () => {
         Venture For You
       </p>
       {/* Responsive button */}
-      <Button className="bg-gradient-to-tr from-[#5AC35A] to-[#00AE76] w-fit text-sm sm:text-base px-4 py-2">
+      <Button className="bg-gradient-to-tr from-[#5AC35A] to-[#00AE76] w-fit text-sm sm:text-base md:text-xl lg:text-2xl xl:text-2xl 4k:text-6xl px-4 py-2 4k:px-8 4k:py-10 4k:rounded-md">
         Start Your Journey
       </Button>
     </div>
