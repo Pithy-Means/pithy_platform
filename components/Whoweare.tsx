baee08f0c4738cd257f64822388a0805ce87bb29
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-01-09 09:05:18 +0000

    Revert "Enhance Whoweare component: adjust text sizes for improved readability on larger screens"
    This reverts commit afc61e24063ac2709974ab88f2dd4ede48fa3e96.

--- components/Whoweare.tsx
+++ components/Whoweare.tsx
@@ -8,10 +8,10 @@ const Whoweare = () => {
       <div className="flex flex-col lg:flex-row justify-between space-y-10 lg:space-y-0 lg:space-x-16 xl:space-x-20 4k:space-x-32 w-full">
         <div className="flex flex-col w-2/3 space-y-4">
           <TitleDot title={"Who We Are"}  />
-          <p className="capitalize text-black text-base lg:text-lg xl:text-lg 4k:text-4xl">
+          <p className="capitalize text-black text-base lg:text-lg xl:text-2xl 4k:text-4xl">
             Empowering informed decisions for a fulfing career.
           </p>
-          <p className="text-black text-base md:text-lg lg:text-xl xl:text-xl 4k:text-5xl">
+          <p className="text-black  text-base md:text-lg lg:text-xl xl:text-2xl 4k:text-5xl">
             At pithy means, we believe that everyone deserves to find their
             perfect fit. our mission is to guide students, professionals, and
             business managers towards their most suitable study, professional,
@@ -22,25 +22,25 @@ const Whoweare = () => {
               <div className="bg-[#5AC35A] w-fit text-black font-extrabold px-2 rounded-md text-base md:text-lg lg:text-xl xl-text-2xl 4k:text-5xl 4k:py-4 4k:mt-4">
                 Our Story
               </div>
-              <p className="capitalize text-black text-base md:text-lg lg:text-xl xl:text-xl 4k:text-5xl">
+              <p className="capitalize text-black text-base md:text-lg lg:text-xl xl:text-2xl 4k:text-5xl">
                 Founded in the US and Uganda, our team saw the need to prevent:
               </p>
               <div className="flex flex-col ml-4 lg:space-y-2 xl:space-y-2 4k:space-y-6">
                 <div className="flex space-x-2 items-center">
                   <div className="border-2 border-[#5AC35A] rotate-45 h-1 w-1 p-0.5 xl:h-2 xl:w-2 4k:h-4 4k:w-4 xl:p-1 4k:p-2"></div>
-                  <p className="capitalize text-black text-base md:text-lg lg:text-xl xl:text-xl 4k:text-5xl">
+                  <p className="capitalize text-black text-base md:text-lg lg:text-xl xl:text-2xl 4k:text-5xl">
                     identify natural strengths and interests
                   </p>
                 </div>
                 <div className="flex space-x-2 items-center">
                   <div className="border-2 border-[#5AC35A] rotate-45 p-0.5 h-1 w-1 xl:h-2 xl:w-2 4k:h-4 4k:w-4 xl:w-2 xl:p-1 4k:p-2"></div>
-                  <p className="capitalize text-black text-base md:text-lg lg:text-xl xl:text-xl 4k:text-5xl">
+                  <p className="capitalize text-black text-base md:text-lg lg:text-xl xl:text-2xl 4k:text-5xl">
                     Explore suitable career and business paths
                   </p>
                 </div>
                 <div className="flex space-x-2 items-center">
                   <div className="border-2 border-[#5AC35A] rotate-45 p-0.5 h-1 w-1 xl:h-2 xl:w-2 4k:h-4 4k:w-4 xl:p-1 4k:p-2"></div>
-                  <p className="capitalize text-black text-base md:text-lg lg:text-xl xl:text-xl 4k:text-5xl xl:mb-2 4k:mb-4">
+                  <p className="capitalize text-black text-base md:text-lg lg:text-xl xl:text-2xl 4k:text-5xl xl:mb-2 4k:mb-4">
                     Make informed decisions
                   </p>
                 </div>
@@ -50,7 +50,7 @@ const Whoweare = () => {
               <div className="bg-[#5AC35A] w-fit text-black font-extrabold px-2 rounded-md text-base md:text-lg lg:text-xl xl-text-2xl 4k:text-5xl 4k:py-4 4k:mt-4">
                 Our vision
               </div>
-              <p className="capitalize text-black text-base md:text-lg lg:text-xl xl:text-xl 4k:text-5xl">
+              <p className="capitalize text-black text-base md:text-lg lg:text-xl xl:text-2xl 4k:text-5xl">
                 A world where everyone thrives in their chosen path.
               </p>
               <Button className="text-white bg-gradient-to-r from-[#5AC35A] to-[#00AE76] w-fit text-base md:text-lg lg:text-xl xl-text-2xl 4k:text-5xl 4k:p-10 4k:mt-4">
