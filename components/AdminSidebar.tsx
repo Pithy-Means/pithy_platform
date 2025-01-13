9ba1cf7be7e03595f651655b0a753830cba34d68
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-01-13 18:35:44 +0000

    Add "Add Job" link to AdminSidebar for job management

--- components/AdminSidebar.tsx
+++ components/AdminSidebar.tsx
@@ -8,17 +8,18 @@ const links = [
   { name: "Home", path: "/admin" },
   { name: "Add Course", path: "/admin/addcourse" },
   { name: "Add Questions", path: "/admin/add_question" },
+  { name: "Add Job", path: "/admin/add_job" },
 ];
 
 const AdminSidebar: React.FC = () => {
   const pathname = usePathname();
 
   return (
-    <aside className="h-screen w-64 bg-gray-800 text-white flex flex-col">
+    <aside className="h-full w-64 bg-gray-800 text-white flex flex-col py-8">
       <div className="p-4 text-lg font-bold border-b border-gray-700">
         Admin Panel
       </div>
-      <nav className="mt-4 flex-1">
+      <nav className="flex-1">
         {links.map((link) => (
           <Link key={link.path} href={link.path}>
             <a
