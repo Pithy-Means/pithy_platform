3781e88660cc2228160e88d9f66842098c6adfcc
Doreen Ikilai <dornkaizen@gmail.com>
2025-01-14 11:43:04 +0000

    Refactor ConditionalLayout component: simplify pathname handling and clean up commented code

--- components/ConditionalLayout.tsx
+++ components/ConditionalLayout.tsx
@@ -4,14 +4,15 @@ import { usePathname } from "next/navigation";
 import Navbar from "@/components/Navbar";
 
 const ConditionalLayout = ({ children }: { children: React.ReactNode }) => {
-  const pathname = usePathname() || '/';
-  const normalizePathname = pathname.replace(/\/$/, "");
+  const pathname = usePathname();
+  // const normalizePathname = pathname.replace(/\/$/, "");
 
   // Determine if the current pathname matches the desired routes
-  const shouldDisplayNavbar = ['/', '/about', '/contact', '/how-it-works'].includes(normalizePathname);
+  const shouldDisplayNavbar = ['/', '/about', '/contact', '/how-it-works'].includes(pathname);
   return (
     <div>
       {shouldDisplayNavbar ? <Navbar /> : null}  
+      {/* {shouldDisplayNavbar && <Navbar />} */}
       {/* Render nothing if not on the specified routes */}
       {children}
     </div>
