63b16a04fe510c29ff2a9172a0e0fd86167b8015
Doreen Ikilai <dornkaizen@gmail.com>
2025-01-17 18:25:11 +0000

    Refactor ShareSomething component for improved layout and integrate PostsProvider

--- components/ShareSomething.tsx
+++ components/ShareSomething.tsx
@@ -1,42 +1,44 @@
 "use client";
 
 import React from "react";
-// import Image from "next/image";
 import Posts from "./Posts";
 import { CircleUserRound } from "lucide-react";
-// import InputContact from "./InputContact";
 import { Button } from "./ui/button";
+import { PostsProvider } from '@/context/PostContext'
 
 const ShareSomething = () => {
   return (
-    <div className="flex flex-col w-full no-scrollbar  max-h-screen md:mt-2 justify-center">
-      {/* Header Section */}
-      <div className="flex flex-col bg-white text-black rounded h-16  sm:mx-4 mx-0">
-        <div className="flex items-center w-full space-x-4">
-          {/* Icon */}
-          <CircleUserRound size={32} className="flex-shrink-0" />
+    <PostsProvider>
+      <div className="flex flex-col no-scrollbar items-center justify-center min-h-screen">
+        {/* Header Section */}
+        <div className="flex flex-col bg-white text-black rounded-lg shadow-md p-2 w-full mt-2  ">
+          <div className="flex items-center justify-center">
+            {/* Icon */}
+            <CircleUserRound size={32} className="flex-shrink-0 text-gray-600 hidden md:block" />
 
-          {/* Input */}
-          <input
-            type="text"
-            placeholder="Share something"
-            className="border-2 border-gray-300 rounded flex-1 py-1.5 px-4 sm:w-fit " 
-            value="" // Add the value property
-            onChange={() => {}} // Open modal when the input is clicked
-          />
+            {/* Input */}
+            <input
+              type="text"
+              placeholder="Share something"
+              className="border border-gray-300 rounded-lg flex-1 py-1 mx-2  focus:outline-none focus:ring-2 focus:ring-[#5AC35A]  "
+              value="" // Add the value property
+              onChange={() => { }} // Open modal when the input is clicked
+            />
 
-          {/* Button */}
-          <Button className="bg-gradient-to-t from-[#5AC35A] to-[#00AE76] text-white rounded-lg py-2 px-6 flex-shrink-0">
-            Post
-          </Button>
+            {/* Button */}
+            <Button className="bg-gradient-to-t from-[#5AC35A] to-[#00AE76] text-white rounded-lg py-1 px-2 flex-shrink-0">
+              Post
+            </Button>
+          </div>
+        </div>
+
+        {/* Scrollable Posts Section */}
+        <div className="flex-1  w-full no-scrollbar mt-4 ">
+          <Posts />
         </div>
       </div>
 
-      {/* Scrollable Posts Section */}
-      <div className="flex-1 space-y-auto no-scrollbar h-full  sm:px-2  sm:mt-10">
-        <Posts />
-      </div>
-    </div>
+    </PostsProvider>
   );
 };
 
