7cf65a7d7eabd446c3788da87e6c05379cc8b540
Doreen Ikilai <dornkaizen@gmail.com>
2025-01-17 18:24:45 +0000

    Enhance Posts component with infinite scroll and loading state management

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import { usePosts } from "@/lib/hooks/usePosts";
 import {
   deletePost,
@@ -14,11 +14,13 @@ import { CommentPost, PostWithUser } from "@/types/schema";
 import usePostInitialization from "@/lib/hooks/usePostInitialization";
 import PostItem from "./PostItem";
 import { UserContext } from "@/context/UserContext";
+import { PostsContext, usePost } from "@/context/PostContext";
+import SkeletonLoader from "./SkeletonLoader";
 
 const Posts = () => {
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
   const fetchedPosts = usePosts();
-  const [posts, setPosts] = useState<PostWithUser[]>([]);
+  // const [posts, setPosts] = useState<PostWithUser[]>([]);
   const [comments, setComments] = useState<{ [key: string]: CommentPost[] }>(
     {}
   );
@@ -31,6 +33,22 @@ const Posts = () => {
   const [repostContent, setRepostContent] = useState<{ [key: string]: string }>(
     {}
   );
+
+  // creating a reference for the last post
+  // const { posts, setPosts, fetchMorePosts, hasMore, loading, setLoading } = useContext(PostsContext);
+  const { posts, setPosts, fetchMorePosts, setLoading,  hasMore, loading } = usePost();
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  const lastPostRef = (node: HTMLDivElement | null) => {
+    if (loading) return;
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && hasMore) {
+        fetchMorePosts();
+      }
+    });
+    if (node) observer.current.observe(node);
+  };
 
   const handleRepost = async (post: PostWithUser) => {
     try {
@@ -117,6 +135,12 @@ const Posts = () => {
     setLoading
   );
 
+  useEffect(() => {
+    if (fetchedPosts.length > 0) {
+      setPosts((prev) => [...prev, ...fetchedPosts]);
+    };
+  }, [fetchedPosts, setPosts]);
+
   const handleDelete = async (postId: string) => {
     try {
       await deletePost(postId);
@@ -162,99 +186,67 @@ const Posts = () => {
   };
 
   return (
-    <div className="flex flex-col gap-4 text-black w-full md:mt-6 pr-10">
+    <div className="flex flex-col gap-4 text-black md:mt-6 sm:pr-2 overflow-y-auto  space-y-4">
+      {/* Skeleton Loader */}
       {loading ? (
-        <div className="py-4 flex flex-col space-y-4 justify-center">
-          <div className="border border-gray-300 shadow rounded-md p-4 w-full">
-            <div className="animate-pulse flex flex-col space-y-4">
-              <div className="flex space-x-4">
-                <div className="rounded-full bg-slate-700 h-8 w-8"></div>
-                <div className="flex flex-col space-y-2 w-full">
-                  <div className="h-2 bg-slate-700 rounded w-1/2"></div>
-                  <div className="h-1.5 bg-slate-700 rounded w-1/3"></div>
-                </div>
-              </div>
-              <div className="flex-1 space-y-6 py-1">
-                <div className="space-y-3">
-                  <div className="grid grid-cols-3 gap-4">
-                    <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                    <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                    <div className="h-2 bg-slate-700 rounded"></div>
-                  </div>
-                  <div className="h-16 bg-slate-800 rounded"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="border border-gray-300 shadow rounded-md p-4 w-full">
-            <div className="animate-pulse flex flex-col space-y-4">
-              <div className="flex space-x-4">
-                <div className="rounded-full bg-slate-700 h-8 w-8"></div>
-                <div className="flex flex-col space-y-2 w-full">
-                  <div className="h-2 bg-slate-700 rounded w-1/2"></div>
-                  <div className="h-1.5 bg-slate-700 rounded w-1/3"></div>
-                </div>
-              </div>
-              <div className="flex-1 space-y-6 py-1">
-                <div className="space-y-3">
-                  <div className="grid grid-cols-3 gap-4">
-                    <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                    <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                    <div className="h-2 bg-slate-700 rounded"></div>
-                  </div>
-                  <div className="h-16 bg-slate-800 rounded"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="border border-gray-300 shadow rounded-md p-4 w-full">
-            <div className="animate-pulse flex flex-col space-y-4">
-              <div className="flex space-x-4">
-                <div className="rounded-full bg-slate-700 h-8 w-8"></div>
-                <div className="flex flex-col space-y-2 w-full">
-                  <div className="h-2 bg-slate-700 rounded w-1/2"></div>
-                  <div className="h-1.5 bg-slate-700 rounded w-1/3"></div>
-                </div>
-              </div>
-              <div className="flex-1 space-y-6 py-1">
-                <div className="space-y-3">
-                  <div className="grid grid-cols-3 gap-4">
-                    <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                    <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                    <div className="h-2 bg-slate-700 rounded"></div>
-                  </div>
-                  <div className="h-16 bg-slate-800 rounded"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
+        // <div className="py-4 flex flex-col space-y-4 justify-center sm:space-y-2">
+        //   {[...Array(5)].map((_, index) => (
+        //     <div key={index} className="border border-gray-300 shadow rounded-md p-4  sm:p-2">
+        //       <div className="animate-pulse flex flex-col space-y-4 sm:space-y-2 ">
+        //         <div className="flex space-x-4 items-center">
+        //           <div className="rounded-full bg-slate-700 h-8 w-8 sm:h-6 sm:w-6"></div>
+        //           <div className="flex flex-col space-y-2 w-full">
+        //             <div className="h-2 bg-slate-700 rounded w-1/2"></div>
+        //             <div className="h-1.5 bg-slate-700 rounded w-1/3 sm:w-1/2"></div>
+        //           </div>
+        //         </div>
+        //         <div className="flex-1 space-y-6 py-1 sm:space-y-3">
+        //           <div className="space-y-3 sm:space-y-2">
+        //             <div className="grid grid-cols-3 gap-4 sm:gap-2">
+        //               <div className="h-2 bg-slate-700 rounded "></div>
+        //               <div className="h-2 bg-slate-700 rounded "></div>
+        //               <div className="h-2 bg-slate-700 rounded"></div>
+        //             </div>
+        //             <div className="h-16 bg-slate-800 rounded sm:h-12"></div>
+        //           </div>
+        //         </div>
+        //       </div>
+        //     </div>
+        //   ))}
+        // </div>
+        <SkeletonLoader />
       ) : (
-        <>
-          {posts.map((post) => (
-            <PostItem
-              key={post.post_id}
-              post={post}
-              loggedInUserId={user?.user_id || null}
-              likeStatus={
-                post.post_id
-                  ? likeStatus[post.post_id] || { isLiked: false, likeCount: 0 }
-                  : { isLiked: false, likeCount: 0 }
-              }
-              comments={post.post_id ? comments[post.post_id] || [] : []}
-              onLike={handleLike}
-              onDelete={handleDelete}
-              onUpdate={handleUpdate}
-              onAddComment={handleAddComment}
-              onRepost={handleRepost}
-            />
+        <div className="space-y-4 sm:space-y-2">
+          {posts.map((post, index) => (
+            <div
+              key={post.post_id || index}
+              ref={posts.length === index + 1 ? lastPostRef : null} // Add the ref to the last post
+            >
+              <PostItem
+                key={post.post_id}
+                post={post}
+                loggedInUserId={user?.user_id || null}
+                likeStatus={
+                  post.post_id
+                    ? likeStatus[post.post_id] || { isLiked: false, likeCount: 0 }
+                    : { isLiked: false, likeCount: 0 }
+                }
+                comments={post.post_id ? comments[post.post_id] || [] : []}
+                onLike={handleLike}
+                onDelete={handleDelete}
+                onUpdate={handleUpdate}
+                onAddComment={handleAddComment}
+                onRepost={handleRepost}
+              />
+            </div>
           ))}
-        </>
-      )}
+
+          {/* No More Posts */}
+          {!loading && !hasMore && (
+            <div className="text-center text-gray-500 py-4">No more posts</div>
+          )}
+        </div>
+      )};
     </div>
   );
 };
