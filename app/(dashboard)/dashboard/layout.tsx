20f52d1895ef27652c83679044adb33b159349a0
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-01-19 19:16:53 +0000

    Implement authentication check in DashboardLayout; redirect to sign-in if user is not authenticated

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -2,29 +2,39 @@
 
 import DashboardNavBar from "@/components/dashboard_navBar";
 import OverView from "@/components/OverView";
-import { UserProvider, UserContext } from "@/context/UserContext";
-import { useContext } from "react";
+import { useAuthStore } from "@/lib/store/useAuthStore";
+import { UserInfo } from "@/types/schema";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <UserProvider>
-      <DashboardContent>{children}</DashboardContent>
-    </UserProvider>
-  );
+  return <DashboardContent>{children}</DashboardContent>;
 }
 
 function DashboardContent({ children }: { children: React.ReactNode }) {
-  const { user } = useContext(UserContext);
+  const { user, isAuthenticated } = useAuthStore(
+    (state) => state as { user: UserInfo; isAuthenticated: boolean }
+  );
+  const router = useRouter();
+
+  useEffect(() => {
+    // Check if user is authenticated when the component mounts
+    if (!isAuthenticated) {
+      router.push("/signIn");
+    }
+  }, [isAuthenticated, router]);
+
+  if (!isAuthenticated) {
+    return null; // Don't render anything until the redirection is complete
+  }
 
   return (
     <div className="relative">
-      <DashboardNavBar
-        user={user ? user.lastname?.charAt(0).toUpperCase() : "G"}
-      >
+      <DashboardNavBar user={user?.user.firstname?.charAt(0)?.toUpperCase() ?? ""}>
         <OverView>{children}</OverView>
       </DashboardNavBar>
     </div>
