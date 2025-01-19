30c3b22671016390a777e776568d102e2316b4ed
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-01-19 19:21:03 +0000

    Refactor Payment components: integrate useAuthStore for user state management, enhance loading states, and improve payment status handling

--- components/PayStatus.tsx
+++ components/PayStatus.tsx
@@ -1,29 +1,69 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { useAuthStore } from "@/lib/store/useAuthStore";
+import { useCourseStore } from "@/lib/store/courseStore";
+import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { UserInfo } from "@/types/schema";
 
 const PaymentStatus = () => {
   const searchParams = useSearchParams();
   const transaction_id = searchParams.get("transaction_id");
   const status = searchParams.get("status");
   const [message, setMessage] = useState("Processing payment...");
+  const [loading, setLoading] = useState<boolean>(false);
   const [messageStyle, setMessageStyle] = useState("text-gray-700");
   const router = useRouter();
+  // Get user info from the store
+  const { user } = useAuthStore((state) => state as UserInfo);
+  // Get the setLocked function from the store
+  const { setLocked } = useCourseStore();
 
   useEffect(() => {
     if (status) {
       const verifyPayment = async () => {
+        setLoading(true);
         try {
-          const response = await fetch(`/api/proxy-flutterwave/payment-status?transaction_id=${transaction_id}`);
+          const response = await fetch(
+            `/api/proxy-flutterwave/payment-status?transaction_id=${transaction_id}`
+          );
           const data = await response.json();
 
           console.log("Payment verification data", data);
 
-          if (data.success === true) {
-            setMessage("Payment successful! Thank you for your purchase.");
-            setMessageStyle("text-green-600 bg-green-50 border border-green-200");
-            router.replace("/dashboard/courses");
+          if (data.success) {
+            console.log("Payment update", data.payment);
+            console.log("Course after payment", data.course);
+
+            const userName = `${user?.user.lastname} ${user?.user.firstname}`;
+            const isStudent = data.course.students.includes(userName) && data.course.student_email.includes(user?.user.email);
+
+            console.log("userName", userName);
+
+            console.log("data.course.students", data.course.students);
+            console.log("data.course.student_email", data.course.student_email);
+
+            console.log("user?.email", user?.user.email);
+
+            console.log("data.course.students.includes(userName)", data.course.students.includes(userName));
+            console.log("data.course.student_email.includes(user?.email)", data.course.student_email.includes(user?.user.email));
+
+            console.log("isStudent", isStudent);
+
+            if (isStudent) {
+
+              setMessage("You have already purchased this course.");
+              setMessageStyle(
+                "text-green-600 bg-green-50 border border-green-200"
+              );
+
+              setLocked(false);
+
+              // Redirect after a slight delay
+              setTimeout(() => {
+                router.push("/dashboard/courses");
+              }, 2000);
+            } 
           } else {
             setMessage("Payment failed. Please try again.");
             setMessageStyle("text-red-600 bg-red-50 border border-red-200");
@@ -32,14 +72,30 @@ const PaymentStatus = () => {
           setMessage(
             "An error occurred while verifying the payment. Please try again later."
           );
-          setMessageStyle("text-yellow-600 bg-yellow-50 border border-yellow-200");
+          setMessageStyle(
+            "text-yellow-600 bg-yellow-50 border border-yellow-200"
+          );
           console.error("Verification error:", error);
+          setLoading(false);
+        } finally {
+          setLoading(false);
         }
       };
 
       verifyPayment();
     }
-  }, [transaction_id, router, status]);
+  }, [transaction_id, status, router, setLocked, user?.user.lastname, user?.user.firstname, user?.user.email]); // Ensure dependencies are up-to-date
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="max-w-md p-6 rounded-lg shadow-md text-green-700">
+          <h1 className="text-xl font-semibold mb-2">Payment Status</h1>
+          <p>Processing payment...</p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
