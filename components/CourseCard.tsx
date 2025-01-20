61ae90e8f02bac244f13601e3fb40553c1172830
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-01-20 19:42:59 +0000

    Enhance CourseCard component: integrate user enrollment checks and improve course display logic for better user experience

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -1,26 +1,36 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 import { MdAccessTimeFilled } from "react-icons/md";
 import { FaBookReader } from "react-icons/fa";
 import { useRouter } from "next/navigation";
-import { Courses } from "@/types/schema";
+import { Courses, UserInfo } from "@/types/schema";
 import PaymentButton from "./PaymentButton";
 import { useCourseStore } from "@/lib/store/courseStore";
+import { useAuthStore } from "@/lib/store/useAuthStore";
 
-const CourseCard: React.FC<{courses: Courses[]}> = ({ courses }) => {
+const CourseCard: React.FC<{ courses: Courses[] }> = ({ courses }) => {
   const router = useRouter();
-  const [modalMessage, setModalMessage] = useState<string | null>(null);
-  const { isLocked } = useCourseStore();
+  const { isLocked, setLocked } = useCourseStore();
+  const { user } = useAuthStore((state) => state as UserInfo);
 
   const handleViewMore = (course: Courses) => {
     router.push(`/dashboard/courses/${course.course_id}`);
   };
 
+  const name = `${user?.user.lastname} ${user?.user.firstname}`;
+  console.log("User", user.user);
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
       {courses.map((course) => {
+        const isStudent = course.students?.includes(name);
+        const isStudentEmail = course.student_email?.includes(user?.user.email);
+        const isEnrolled = isStudent && isStudentEmail;
+
+        if (isLocked) setLocked(false);
+
         return (
           <div
             key={course.course_id}
@@ -42,68 +52,69 @@ const CourseCard: React.FC<{courses: Courses[]}> = ({ courses }) => {
                   <p className="text-red-600 font-bold text-lg mb-2">
                     This course is locked.
                   </p>
-                  <p className="text-gray-600 mb-4">{modalMessage}</p>
-                  <PaymentButton course={{
-                    course_id: course.course_id,
-                    title: course.title,
-                    price: course.price,
-                  }} />
+                  <PaymentButton
+                    course={{
+                      course_id: course.course_id,
+                      title: course.title,
+                      price: course.price,
+                    }}
+                  />
                 </div>
               ) : (
-                <>
-                  <div className="flex flex-col space-y-1 border-b-slate-300 border-b-4">
-                    <p className="text-black font-bold text-lg mb-2">
-                      {course.title}
-                    </p>
-                    <div className="flex items-center justify-between pb-3">
-                      <p className="text-gray-600 flex gap-1 items-center">
-                        <MdAccessTimeFilled
-                          size={20}
-                          className="text-orange-600 gap-1"
-                        />
-                        {course.duration}
+                // If course is not locked and user is enrolled
+                isEnrolled ? (
+                  <>
+                    {/* Display course details if enrolled */}
+                    <div className="flex flex-col space-y-1 border-b-slate-300 border-b-4">
+                      <p className="text-black font-bold text-lg mb-2">
+                        {course.title}
                       </p>
-                      <p className="text-gray-600 flex gap-2">
-                        <FaBookReader size={20} className="text-orange-600" />
-                        {course.students?.length || 0} Learners
-                      </p>
+                      <div className="flex items-center justify-between pb-3">
+                        <p className="text-gray-600 flex gap-1 items-center">
+                          <MdAccessTimeFilled
+                            size={20}
+                            className="text-orange-600 gap-1"
+                          />
+                          {course.duration}
+                        </p>
+                        <p className="text-gray-600 flex gap-2">
+                          <FaBookReader size={20} className="text-orange-600" />
+                          {course.students?.length || 0} Learners
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                  <div className="flex flex-row justify-between items-center">
-                    <p className="text-green-600 text-base">
-                      {course.price} UGX
+                    <div className="flex flex-row justify-between items-center">
+                      <p className="text-green-600 text-base">
+                        {course.price} UGX
+                      </p>
+                      <button
+                        type="button"
+                        onClick={() => handleViewMore(course)}
+                        className="text-black/85 font-semibold text-base hover:border rounded-md hov:bg-green-600/100 transition px-2 py-1"
+                      >
+                        View more
+                      </button>
+                    </div>
+                  </>
+                ) : (
+                  <div className="flex flex-col items-center justify-center text-center h-full">
+                    <p className="text-red-600 font-bold text-lg mb-2">
+                      You are not enrolled in this course.
                     </p>
-                    <button
-                      type="button"
-                      onClick={() => handleViewMore(course)}
-                      className="text-black/85 font-semibold text-base hover:border rounded-md hov:bg-green-600/100 transition px-2 py-1"
-                    >
-                      View more
-                    </button>
+                    <PaymentButton
+                      course={{
+                        course_id: course.course_id,
+                        title: course.title,
+                        price: course.price,
+                      }}
+                    />
                   </div>
-                </>
+                )
               )}
             </div>
           </div>
         );
       })}
-
-      {/* Modal */}
-      {modalMessage && (
-        <div className="fixed inset-0 bg-black bg-opacity-20 flex items-center justify-center">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
-            <p className="text-lg font-medium text-gray-800">{modalMessage}</p>
-            <div className="mt-4 flex justify-end">
-              <button
-                onClick={() => setModalMessage(null)}
-                className="bg-red-600 text-white px-4 py-2 rounded-md"
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
