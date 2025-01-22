056481edab25f5915cd40f2311603f824ea7956d
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-01-22 19:45:49 +0000

    Refactor PersonSidebar component: improve user data access and type casting

--- components/PersonSidebar.tsx
+++ components/PersonSidebar.tsx
@@ -29,16 +29,16 @@ const PersonSidebar = () => {
           <div className="flex flex-col justify-center items-center -mt-7">
             <div className="bg-white rounded-full p-2">
               <p className="text-black text-2xl font-extrabold">
-                {user?.user.firstname?.charAt(0).toUpperCase() || ""}
+                {user?.firstname?.charAt(0).toUpperCase() || ""}
               </p>
             </div>
             <div className="text-black items-center flex flex-col space-y-1">
               <span className="text-lg font-bold">
-                {user.user.firstname} {user.user.lastname}
+                {user.firstname} {user.lastname}
               </span>
               <button className="text-base bg-gray-400 rounded-sm px-2 py-1">
-                {user?.user.categories?.charAt(0).toUpperCase()}
-                {user?.user.categories?.slice(1)}
+                {user?.categories?.charAt(0).toUpperCase()}
+                {user?.categories?.slice(1)}
               </button>
             </div>
           </div>
