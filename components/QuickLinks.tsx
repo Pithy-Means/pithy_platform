6d58a89bba28bed3fd8c4301e2d44872dc915533
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-02-06 20:40:27 +0000

    Update QuickLinks component for improved styling and accessibility

--- components/QuickLinks.tsx
+++ components/QuickLinks.tsx
@@ -3,43 +3,43 @@ import Link from "next/link";
 const QuickLinks: React.FC = () => {
   return (
     <div className="flex flex-col space-y-4 md:w-1/5 text-center justify-center w-full">
-      <h3 className="text-lg font-bold text-white md:text-base lg:text2xl">Quick Links</h3>
+      <h3 className="text-2xl font-extrabold text-white">Quick Links</h3>
       <nav className="flex flex-col space-y-2 ">
         <Link
           href=""
-          className="text-[12px] md:text-base text-gray-600 hover:text-white transition duration-300 ease-in-out"
+          className="text-[12px] md:text-base text-white hover:text-white transition duration-300 ease-in-out"
         >
           Home
         </Link>
         <Link
           href="/about"
-          className="text-[12px] md:text-base text-gray-600 hover:text-white transition duration-300 ease-in-out"
+          className="text-[12px] md:text-base text-white hover:text-white transition duration-300 ease-in-out"
         >
           About
         </Link>
         <Link
           href="/how-it-works"
-          className="text-[12px] md:text-base text-gray-600 hover:text-white transition duration-300 ease-in-out"
+          className="text-[12px] md:text-base text-white hover:text-white transition duration-300 ease-in-out"
         >
           How It Works
         </Link>
         <Link
           href="/contact"
-          className="text-[12px] md:text-base text-gray-600 hover:text-white transition duration-300 ease-in-out"
+          className="text-[12px] md:text-base text-white hover:text-white transition duration-300 ease-in-out"
         >
           Contact
         </Link>
         <div className="flex lg:space-x-2 text-center justify-center flex-wrap lg:flex-row flex-col">
           <Link
             href="/signIn"
-            className="text-[12px] md:text-base  text-gray-600 hover:text-white transition duration-300 ease-in-out"
+            className="text-[12px] md:text-base  text-white hover:text-white transition duration-300 ease-in-out"
           >
             Login
           </Link>
-          <span className="text-gray-600 hidden lg:block">|</span>
+          <span className="text-white hidden lg:block">|</span>
           <Link
             href="/signUp"
-            className="text-[12px] md:text-base text-gray-600 hover:text-white transition duration-300 ease-in-out"
+            className="text-[12px] md:text-base text-white hover:text-white transition duration-300 ease-in-out"
           >
             Sign Up
           </Link>
