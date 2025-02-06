8f56b21f2cc700176fb1a64864894737d688cd24
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-02-06 06:55:16 +0000

    Refactor payment initiation route to include currency in request body and update payment options

--- app/(api)/api/proxy-flutterwave/initiate/route.ts
+++ app/(api)/api/proxy-flutterwave/initiate/route.ts
@@ -1,68 +1,84 @@
-  import { NextResponse } from "next/server";
-  import env from "@/env";
-  import { createAdminClient } from "@/utils/appwrite";
-  import { db, paymentCollection } from "@/models/name";
-  import { generateValidPostId } from "@/lib/utils";
+import { NextResponse } from "next/server";
+import env from "@/env";
+import { createAdminClient } from "@/utils/appwrite";
+import { db, paymentCollection } from "@/models/name";
+import { generateValidPostId } from "@/lib/utils";
+import { PaymentData } from "@/types/schema";
 
-  export async function POST(req: Request) {
-    try {
-      // Parse the request body
-      const body = await req.json(); // Use req.json() to parse the body
-      const { tx_ref, amount, email, name, paymentId, course_choice, user_id, phone_number } = body;
-      const payId = generateValidPostId(paymentId);
+export async function POST(req: Request) {
+  try {
+    // Parse the request body
+    const body: PaymentData = await req.json(); // Use req.json() to parse the body
+    const {
+      tx_ref,
+      amount,
+      email,
+      name,
+      payment_id,
+      course_choice,
+      user_id,
+      phone_number,
+      currency,
+    } = body;
+    const payId = generateValidPostId(payment_id);
 
-      const response = await fetch("https://api.flutterwave.com/v3/payments", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${env.payment.secret}`,
-          "Content-Type": "application/json",
+    const response = await fetch("https://api.flutterwave.com/v3/payments", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${env.payment.secret}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        tx_ref,
+        amount,
+        currency,
+        redirect_url: "http://localhost:3000/payment-status",
+        payment_options:
+          "card, ussd, mpesa, mobile_money_franco, qr, mobile_money_uganda, mobile_money_ghana, mobile_money_rwanda, mobile_money_tanzania, bank_transfer, opay, enaira, googlepay, applepay, capitec",
+        customer: {
+          email,
+          name,
         },
-        body: JSON.stringify({
-          tx_ref,
-          amount,
-          currency: "UGX",
-          redirect_url: "http://localhost:3000/payment-status",
-          payment_options: "card, banktransfer",
-          customer: {
-            email,
-            name,
-          },
-          customizations: {
-            title: "Course",
-            description: "Payment for course",
-            logo: "http://localhost:3000/assets/logo.png",
-          },
-        }),
-      });
+        customizations: {
+          title: "Course",
+          description: "Payment for course",
+          logo: "http://localhost:3000/assets/logo.png",
+        },
+      }),
+    });
 
-      console.log("response", response);
+    console.log("response", response);
 
-      const data = await response.json();
-      console.log("data from initiate", data);
+    const data = await response.json();
+    console.log("data from initiate", data);
 
-      if (data.status === "success") {
-        const { databases } = await createAdminClient();
+    if (data.status === "success") {
+      const { databases } = await createAdminClient();
 
-        const storePayment = await databases.createDocument(db, paymentCollection, payId, {
+      const storePayment = await databases.createDocument(
+        db,
+        paymentCollection,
+        payId,
+        {
           user_id,
           tx_ref,
           amount,
           phone_number,
-          network : "flutterwave",
+          currency,
           email,
-          currency: "UGX",
           payment_id: payId,
           course_choice,
           status: "pending",
-        });
+        }
+      );
 
-        console.log("storePayment", storePayment);
-        return NextResponse.json({ ...storePayment, link: data.data.link });
-      } else {
-        return NextResponse.json(data);
-      }
-    } catch (error) {
-      console.error("Error processing request:", error);
-      return NextResponse.json({ error: "An error occurred" });
+      console.log("storePayment", storePayment);
+      return NextResponse.json({ ...storePayment, link: data.data.link });
+    } else {
+      return NextResponse.json(data);
     }
+  } catch (error) {
+    console.error("Error processing request:", error);
+    return NextResponse.json({ error: "An error occurred" });
   }
+}
