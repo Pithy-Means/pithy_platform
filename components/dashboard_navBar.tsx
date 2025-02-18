87b85641f9775f648fe7dcaa34edd8ff5c12c47f
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-02-18 09:53:07 +0000

    Refactor DashboardNavBar layout for improved spacing and responsiveness

--- components/dashboard_navBar.tsx
+++ components/dashboard_navBar.tsx
@@ -11,12 +11,12 @@ const DashboardNavBar: React.FC<DashboardNavBarProps> = ({ user, children }) => 
 
   return (
     <div className="bg-white/90">
-      <nav className="flex h-20 w-full bg-white justify-evenly items-center px-4">
+      <nav className="flex h-20 w-full bg-white justify-between items-center px-10">
         <div className="text-2xl font-bold text-black">
           <Logo />
         </div>
         <div className="hidden lg:block">
-          <div className="hidden relative lg:flex space-x-4 items-center bg-gray-300 border rounded-lg">
+          <div className="hidden relative lg:flex space-x-4 items-center bg-gray-300 border rounded-lg w-full p-2">
             <input
               className="bg-gray-300 outline-none w-full p-2 pl-10 text-black/65"
               placeholder="Search Course"
@@ -25,12 +25,14 @@ const DashboardNavBar: React.FC<DashboardNavBarProps> = ({ user, children }) => 
             <Search color="#7f7676" />
           </div>
         </div>
-        <div>
-          <IoMdNotificationsOutline className="text-black h-8 w-8" />
-        </div>
-        <div className="flex items-center gap-x-2">
-          <div className="bg-gray-600 py-1 px-2 rounded-full border border-black">
-            {user}
+        <div className="flex items-center gap-x-8 mr-0 lg:mr-10">
+          <div>
+            <IoMdNotificationsOutline className="text-black h-8 w-8" />
+          </div>
+          <div className="flex items-center gap-x-2">
+            <div className="bg-black py-1.5 px-3 rounded-full border border-white shadow-md">
+              {user}
+            </div>
           </div>
         </div>
       </nav>
