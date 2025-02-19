d2285aba6edb92db09706b60d529198a8cd32355
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-02-19 09:18:55 +0000

    Refactor Dashboard component by removing unused imports and adjusting sidebar width for improved layout

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,32 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
-import React, { useContext, useState } from "react";
-import dynamic from "next/dynamic";
+import React from "react";
+// import dynamic from "next/dynamic";
 import ShareSomething from "@/components/ShareSomething";
-import { BriefcaseBusiness, School } from "lucide-react";
-import { GoHome } from "react-icons/go";
-import { HiMiniClipboardDocumentList } from "react-icons/hi2";
-import { MdOutlineAddCircle } from "react-icons/md";
-
-import Link from 'next/link';
-import Modal from "@/components/Modal";
-import CreatePosts from "@/components/createPosts";
-import { PostWithUser } from "@/types/schema";
-import { UserContext } from "@/context/UserContext";
+import PersonSidebar from "@/components/PersonSidebar";
 
 // Lazy load the sidebar component
-const PersonSidebar = dynamic(() => import("@/components/PersonSidebar"), { ssr: false });
+// const PersonSidebar = dynamic(() => import("@/components/PersonSidebar"), { ssr: true });
 
 function Dashboard() {
   return (
-    <div className="flex  flex-col lg:flex-row  lg:space-x-4 w-full relative">
+    <div className="flex flex-col lg:flex-row lg:space-x-4 w-full relative">
       {/* Main content area */}
       <div className="px-4 sm:px-6 lg:px-8 w-full lg:w-[65%]">
         <ShareSomething />
       </div>
       {/* Sidebar for larger screens */}
-      <div className="hidden lg:flex flex-col space-y-4 h-screen w-1/4  lg:py-6 lg:px-4  z-10">
+      <div className="hidden lg:flex flex-col space-y-4 h-screen w-1/3 lg:py-6 lg:px-4 z-10">
         <PersonSidebar />
       </div>
     </div>
