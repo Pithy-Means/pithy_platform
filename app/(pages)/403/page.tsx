0f881733eedd0b67ab45e9195cfa8628b4b744ff
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-03-20 19:15:00 +0000

    Add metadata for 403 page to enhance SEO and social sharing

--- app/(pages)/403/page.tsx
+++ app/(pages)/403/page.tsx
@@ -1,4 +1,32 @@
 import Custom403 from "@/components/403";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title:  "Forbidden",
+  description: "You are not allowed to access this page.",
+  keywords: ["Pithy Means", "forbidden", "access denied"],
+  openGraph: {
+    title: "Forbidden",
+    description: "You are not allowed to access this page.",
+    url: "https://www.pithymeans.com/403",
+    siteName: "Pithy Means",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Pithy Means - Empowering Individuals",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Forbidden",
+    description: "You are not allowed to access this page.",
+    images: ["/opengraph-image.png"],
+  },
+}
 
 export default function Page() {
   return <Custom403 />;
