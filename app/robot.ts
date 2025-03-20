d473e2b0f57621a867b8a8159191f3018d30b109
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-03-20 19:16:41 +0000

    Update robots.txt to disallow additional paths for improved security

--- app/robot.ts
+++ app/robot.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{
       userAgent: "*",
-      disallow: ["/admin", "/api"],
+      disallow: ["/admin", "/api", "/_next", "/public", "/403"],
       allow: ["/"],
     }],
     sitemap: `${env.endpoint?.base}/sitemap.xml`,
