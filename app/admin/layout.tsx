9d993cdd1f9bdecf84cbab4f60cd387ac90e1f70
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-04-04 05:39:57 +0000

    Refactor the code to use a more modern approach

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,28 +1,18 @@
-"use client";
-
 import AdminSidebar from "@/components/AdminSidebar";
 import DashboardNavBar from "@/components/dashboard_navBar";
-import { UserProvider } from "@/context/UserContext";
-import { useAuthStore } from "@/lib/store/useAuthStore";
-import { UserInfo } from "@/types/schema";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <UserProvider>
-      <DashboardContent>{children}</DashboardContent>
-    </UserProvider>
-  );
+  return <DashboardContent>{children}</DashboardContent>;
 }
 
 function DashboardContent({ children }: { children: React.ReactNode }) {
-  const { user } = useAuthStore((state) => state as unknown as UserInfo);
   return (
     <div className="relative">
-      <DashboardNavBar user={user?.firstname?.charAt(0)?.toUpperCase() ?? ""}>
+      <DashboardNavBar>
         <div className="flex mt-4 space-x-4">
           <AdminSidebar />
           <main className="flex-1 px-4 bg-gray-50">{children}</main>
