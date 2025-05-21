458876ead35f3083aaa32004afda0fdeb928ef28
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-05-21 08:36:03 +0300

    feat: Add TypeScript declaration for 'next-pwa' module

---
+++ utils/custom.d.ts
@@ -0,0 +1,7 @@
+// custom.d.ts
+declare module 'next-pwa' {
+  import { NextConfig } from 'next';
+  
+  function withPWA(config: NextConfig): (nextConfig: NextConfig) => NextConfig;
+  export default withPWA;
+}
